aed2c93547352d83a034c523a6cb6abd3638a2f1
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-05 00:57:58 -0700

    further development on button
    red
    green
    refactor

--- __tests__/components/ui/button.test.ts
+++ __tests__/components/ui/button.test.ts
@@ -1,24 +1,76 @@
 import Button from "@/components/ui/button/Button";
+import {Loader2} from "lucide-react";
+
 describe('Button',()=>{
+
     test('Button type test',()=>{
         const button = Button({})
-        expect(button?.type).toEqual('button')
+        expect(button?.type)
+            .toEqual('button')
     })
+
     test('Miscellaneous props passed to React Parent', ()=>{
         const button =  Button( { color:"#841584"})
-        expect(button?.props.color).toEqual("#841584")
+        expect(button?.props.color)
+            .toEqual("#841584")
     })
+
     test('Miscellaneous props passed to React Parent', ()=>{
         const button =  Button({})
         console.log({button})
-        expect(button?.props).not.toEqual(expect.objectContaining({color:"#841584"}))
+        expect(button?.props)
+            .not
+            .toEqual(expect.objectContaining({color:"#841584"}))
     })
+
     test('if isLoading is false, then disabled is false', ()=>{
         const button =  Button({isLoading: false})
-        expect(button?.props.disabled).toEqual(false)
+        expect(button?.props.disabled)
+            .toEqual(false)
     })
+
     test('if isLoading is true, then disabled is true', ()=>{
         const button =  Button({isLoading: true})
-        expect(button?.props.disabled).toEqual(true)
+        expect(button?.props.disabled)
+            .toEqual(true)
     })
+
+    test ('if isLoading is false, then do not expect a Loader2 icon to be rendered', ()=>{
+        const button =  Button({isLoading: false})
+        expect(button?.props?.children?.type)
+            .not
+            .toEqual(Loader2)
+    })
+
+    test ('if isLoading is true, then expect a Loader2 icon to be rendered', ()=>{
+        const button =  Button({isLoading: true})
+        expect(button?.props?.children)
+            .toEqual(expect
+                .arrayContaining([expect
+                    .objectContaining({type: Loader2})]))
+    })
+
+    test ('if isLoading is true, then expect styling to be set in the Loader2 element', ()=>{
+        const button =  Button({isLoading: true})
+        const styling = 'mr-2 h-4 w-4 animate-spin'
+        expect(button?.props?.children)
+            .toEqual(expect
+                .arrayContaining([expect
+                    .objectContaining({props: expect
+                            .objectContaining({className:styling})}),]))})
+
+    test ('The children field of the argument should be rendered in the final button', ()=>{
+        const button =  Button({isLoading: true, children: "TK421, why aren't you at your post?"})
+        expect(button?.props?.children)
+            .toEqual(expect
+                .arrayContaining(["TK421, why aren't you at your post?"]))
+    })
+
+    test ('The children field of the argument should be rendered in the final button, different data', ()=>{
+        const button =  Button({isLoading: true, children: "Boring conversation anyway"})
+        expect(button?.props?.children)
+            .toEqual(expect
+                .arrayContaining(["Boring conversation anyway"]))
+    })
+
 })
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,8 +1,12 @@
 import {FC} from "react";
 import {ButtonProps} from "@/components/ui/button/buttonProps";
+import {Loader2} from "lucide-react";
 
 const Button:FC<ButtonProps> = (args: ButtonProps)=>{
-    const {...props} = args
-    return <button disabled={args.isLoading} {...props}></button>
+    const {isLoading, children, ...props} = args
+    return <button disabled={isLoading} {...props}>
+        {isLoading ? <Loader2 className='mr-2 h-4 w-4 animate-spin'/> :null}
+        {children}
+    </button>
 }
 export default Button
