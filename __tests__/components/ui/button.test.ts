7321240ba095b632baa14246b27d00f9e81a92ef
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-05 00:11:23 -0700

    red test for parent props
    define interface for properties of button
    red, green, refactor
    
    green
    
    refactor
    
    green
    
    refeactor
    
    red
    
    update test
    
    green
    
    red
    
    green
    
    refactor

--- __tests__/components/ui/button.test.ts
+++ __tests__/components/ui/button.test.ts
@@ -1,7 +1,24 @@
 import Button from "@/components/ui/button/Button";
 describe('Button',()=>{
-    test('default button content test',()=>{
-        const button =  Button()
-        expect(button.props.children).toEqual('Button')
+    test('Button type test',()=>{
+        const button = Button({})
+        expect(button?.type).toEqual('button')
+    })
+    test('Miscellaneous props passed to React Parent', ()=>{
+        const button =  Button( { color:"#841584"})
+        expect(button?.props.color).toEqual("#841584")
+    })
+    test('Miscellaneous props passed to React Parent', ()=>{
+        const button =  Button({})
+        console.log({button})
+        expect(button?.props).not.toEqual(expect.objectContaining({color:"#841584"}))
+    })
+    test('if isLoading is false, then disabled is false', ()=>{
+        const button =  Button({isLoading: false})
+        expect(button?.props.disabled).toEqual(false)
+    })
+    test('if isLoading is true, then disabled is true', ()=>{
+        const button =  Button({isLoading: true})
+        expect(button?.props.disabled).toEqual(true)
     })
 })
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,4 +1,8 @@
-const Button = ()=>{
-    return <div>Button</div>
+import {FC} from "react";
+import {ButtonProps} from "@/components/ui/button/buttonProps";
+
+const Button:FC<ButtonProps> = (args: ButtonProps)=>{
+    const {...props} = args
+    return <button disabled={args.isLoading} {...props}></button>
 }
 export default Button
