02ab6b1be406fa176c92b6dffea37c9eb072879a
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-07 16:02:13 -0700

    green

---
+++ src/app/pages/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import {FC} from "react";
+
+interface pageProps {}
+
+const Dashboard: FC<pageProps> = ({}) =>{
+    return <><title>dashboard</title></>
+}
+
+export default Dashboard
