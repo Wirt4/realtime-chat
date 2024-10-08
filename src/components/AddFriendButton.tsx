7d9d3cf98ee59522ba37ecc178ab5f44f218657b
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-08 13:30:04 -0700

    styling

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -4,10 +4,15 @@ import {FC} from "react";
 import Button from "@/components/ui/button/Button";
 
 const AddFriendButton: FC = ()=>{
-    return <form>
-        <div>
-        <label>Add a Friend by Email:</label>
-            <input type="text"/>
+    return <form className='max-w-sum'>
+        <label htmlFor='email'
+        className='block text-sm font-medium leading-6 text-gray-900'>Add a Friend by Email:</label>
+        <div className='mt-2 flex gap-4'>
+            <input
+                type="text"
+                placeholder="yourfriend@example.com"
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            />
             <Button>Add</Button>
         </div>
     </form>
