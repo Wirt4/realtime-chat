f0a7f89fc5cfda8dc235d82380c7198efb51c112
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-08 12:22:56 -0700

    Change in specs for Add friend, red test

--- __tests__/pages/(dashboard)/add/page.test.tsx
+++ __tests__/pages/(dashboard)/add/page.test.tsx
@@ -1,39 +1,12 @@
 import '@testing-library/jest-dom'
 import Page from "@/app/(dashboard)/add/page"
-import {getServerSession} from "next-auth"
-import {Auth} from "@/lib/auth"
+import {render} from "@testing-library/react";
 
-jest.mock("next-auth", () => ({
-    getServerSession: jest.fn(),
-}))
+describe('Add Page tests',()=>{
 
-jest.mock("../../../../src/lib/auth.ts", () => ({
-    Auth:{
-        options: jest.fn()
-    }
-}))
-
-describe('Get Server Session test',()=>{
-    let mockGetServerSession: jest.Mock
-    beforeEach(()=>{
-        mockGetServerSession = getServerSession as jest.Mock
-        mockGetServerSession.mockResolvedValue({})
-    })
-    afterEach(()=>{
-        jest.resetAllMocks()
-    })
-
-   test('page should call getServerSession', async ()=>{
-        await Page({})
-       expect(mockGetServerSession).toHaveBeenCalledTimes(1)
-    })
-
-    test ('getServerSession should be called with the auth.Options', async ()=>{
-        const opts = {providers: []}
-        jest.spyOn(Auth, 'options').mockReturnValueOnce(opts)
-
-        await Page({})
-        expect(mockGetServerSession).toHaveBeenCalledWith(opts)
+   test('Document should instruct you to add a friend', async ()=>{
+      render(<Page/>)
+       expect("Add a Friend").toBeInTheDocument()
     })
 
 })
--- src/app/(dashboard)/add/page.tsx
+++ src/app/(dashboard)/add/page.tsx
@@ -1,9 +1,6 @@
 import {FC} from 'react'
-import {getServerSession} from "next-auth"
-import {Auth} from "@/lib/auth"
 
-const Page: FC = async ({}) => {
-    await getServerSession(Auth.options())
+const Page: FC = ({}) => {
     return <></>
 }
 
