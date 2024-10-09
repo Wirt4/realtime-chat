d7bd835bb1deed7c017a1a4aa571ed7a2dd54da6
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-09 08:31:05 -0700

    green

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,10 +1,14 @@
 import {addFriendValidator} from "@/lib/validations/add-friend";
+import {ZodError} from "zod";
 
 export async function  POST(req: Request):Promise<Response> {
     try{
         addFriendValidator.parse(req.body)
         return new Response('shiny happy people')
     }catch(err){
-        return new Response("Invalid request payload", {status: 422})
+        if (err instanceof ZodError){
+            return new Response("Invalid request payload", {status: 422})
+        }
+        return new Response("Invalid request", {status: 400})
     }
 }
