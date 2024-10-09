ec46ce9886f1c050e54d2e1cf45422d5cc8eb192
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-09 15:21:27 -0700

    refactor

--- __tests__/api/friends/add/route.test.ts
+++ __tests__/api/friends/add/route.test.ts
@@ -25,120 +25,126 @@ jest.mock("../../../../src/lib/db",()=>({
 describe('Validate Tests - true verses false', () => {
     let handler: PostFriendsRouteHandler
     beforeEach(()=>{
-        handler = new PostFriendsRouteHandler()
+        handler = new PostFriendsRouteHandler();
     })
     afterEach(()=>{
-        jest.resetAllMocks()
+        jest.resetAllMocks();
     })
     test("email is invalid expect false", async()=>{
         jest.spyOn(handler, 'validateEmail').mockImplementation(()=>{
-            throw new Error('error')
+            throw new Error('error');
         })
         const expected = {message:'Invalid request payload',
             opts: { status: 422 }
         }
 
-       await handler.isValidRequest({email:'stupid invalid email'})
-        expect(handler.errorResponse()).toEqual(expected)
-    })
+       await handler.isValidRequest({email:'stupid invalid email'});
+        expect(handler.errorResponse()).toEqual(expected);
+    });
 
     test("user does not exist", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(false)
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(false);
         const expected = {
             message:'This person does not exist.',
             opts: { status: 400 }
         }
 
 
-       await handler.isValidRequest({email:'example@example.com'})
-        expect(handler.errorResponse()).toEqual(expected)
-    })
+       await handler.isValidRequest({email:'example@example.com'});
+        expect(handler.errorResponse()).toEqual(expected);
+    });
+
     test("session is unauthorized", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(true)
-        jest.spyOn(handler, 'getSession').mockResolvedValue(false)
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(true);
+        jest.spyOn(handler, 'getSession').mockResolvedValue(false);
         const expected = {
             message:'unauthorized',
             opts: { status: 401 }
         }
-        await handler.isValidRequest({email:'example@example.com'})
-        expect(handler.errorResponse()).toEqual(expected)
-    })
+        await handler.isValidRequest({email:'example@example.com'});
+        expect(handler.errorResponse()).toEqual(expected);
+    });
 
     test("user attempts to add self", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(true)
-        jest.spyOn(handler, 'getSession').mockResolvedValue(true)
-        jest.spyOn(handler, 'isSameUser').mockReturnValue(true)
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(true);
+        jest.spyOn(handler, 'getSession').mockResolvedValue(true);
+        jest.spyOn(handler, 'isSameUser').mockReturnValue(true);
         const expected = {
             message:'You cannot add yourself as a friend',
             opts: { status: 400 }
         }
-        await handler.isValidRequest({email:'example@example.com'})
-        expect(handler.errorResponse()).toEqual(expected)
+        await handler.isValidRequest({email:'example@example.com'});
+        expect(handler.errorResponse()).toEqual(expected);
     })
 
     test("the target is already added", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(true)
-        jest.spyOn(handler, 'getSession').mockResolvedValue(true)
-        jest.spyOn(handler, 'isSameUser').mockReturnValue(false)
-        jest.spyOn(handler, 'isAlreadyAdded').mockResolvedValue(true)
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(true);
+        jest.spyOn(handler, 'getSession').mockResolvedValue(true);
+        jest.spyOn(handler, 'isSameUser').mockReturnValue(false);
+        jest.spyOn(handler, 'isAlreadyAdded').mockResolvedValue(true);
         const expected = {
             message:'You\'ve already added this user',
             opts: { status: 400 }
         }
-        await handler.isValidRequest({email:'example@example.com'})
-        expect(handler.errorResponse()).toEqual(expected)
-    })
+        await handler.isValidRequest({email:'example@example.com'});
+        expect(handler.errorResponse()).toEqual(expected);
+    });
 
     test("the target is already added", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(true)
-        jest.spyOn(handler, 'getSession').mockResolvedValue(true)
-        jest.spyOn(handler, 'isSameUser').mockReturnValue(false)
-        jest.spyOn(handler, 'isAlreadyAdded').mockResolvedValue(false)
-        jest.spyOn(handler, 'areAlreadyFriends').mockResolvedValue(true)
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(true);
+        jest.spyOn(handler, 'getSession').mockResolvedValue(true);
+        jest.spyOn(handler, 'isSameUser').mockReturnValue(false);
+        jest.spyOn(handler, 'isAlreadyAdded').mockResolvedValue(false);
+        jest.spyOn(handler, 'areAlreadyFriends').mockResolvedValue(true);
         const expected = {
             message:"You're already friends with this user",
             opts: { status: 400 }
         }
-        await handler.isValidRequest({email:'example@example.com'})
-        expect(handler.errorResponse()).toEqual(expected)
+        await handler.isValidRequest({email:'example@example.com'});
+        expect(handler.errorResponse()).toEqual(expected);
     })
-
-})
+});
 
 describe("error response tests",()=>{
     let handler: PostFriendsRouteHandler
+
     beforeEach(()=>{
         handler = new PostFriendsRouteHandler()
     })
+
     afterEach(()=>{
         jest.resetAllMocks()
     })
+
     test("email is invalid expect false", async()=>{
         jest.spyOn(handler, 'validateEmail').mockImplementation(()=>{
-            throw new Error('error')
+            throw new Error('error');
         })
 
-        const actual = await handler.isValidRequest({email:'stupid invalid email'})
-        expect(actual).toEqual(false)
+        const actual = await handler.isValidRequest({email:'stupid invalid email'});
+        expect(actual).toEqual(false);
     })
+
     test("user does not exist", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(false)
-        const actual = await handler.isValidRequest({email:'example@example.com'})
-        expect(actual).toEqual(false)
-    })
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(false);
+        const actual = await handler.isValidRequest({email:'example@example.com'});
+        expect(actual).toEqual(false);
+    });
+
     test("session is unauthorized", async()=>{
-        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
-        jest.spyOn(handler, 'userExists').mockResolvedValue(true)
-        jest.spyOn(handler, 'getSession').mockResolvedValue(false)
-        const actual = await handler.isValidRequest({email:'example@example.com'})
-        expect(actual).toEqual(false)
-    })
+        jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'});
+        jest.spyOn(handler, 'userExists').mockResolvedValue(true);
+        jest.spyOn(handler, 'getSession').mockResolvedValue(false);
+        const actual = await handler.isValidRequest({email:'example@example.com'});
+        expect(actual).toEqual(false);
+    });
+
     test("user attempts to add self", async()=>{
         jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
         jest.spyOn(handler, 'userExists').mockResolvedValue(true)
@@ -156,6 +162,7 @@ describe("error response tests",()=>{
         const actual = await handler.isValidRequest({email:'example@example.com'})
         expect(actual).toEqual(false)
     })
+
     test("the target is already added", async()=>{
         jest.spyOn(handler, 'validateEmail').mockReturnValue({email:'valid@email.com'})
         jest.spyOn(handler, 'userExists').mockResolvedValue(true)
@@ -166,40 +173,46 @@ describe("error response tests",()=>{
         const actual = await handler.isValidRequest({email:'example@example.com'})
         expect(actual).toEqual(false)
     })
-})
+});
 
 describe("Trigger Pusher tests", ()=>{
     let handler: PostFriendsRouteHandler
     beforeEach(()=>{
         handler = new PostFriendsRouteHandler()
     })
+
     afterEach(()=>{
         jest.resetAllMocks()
     })
+
     test("check the correct arguments have been passed to 'toPusherKey'",async ()=>{
         handler.idToAdd ='1701'
         const spy = jest.spyOn(Utils, 'toPusherKey')
         await handler.triggerPusher()
         expect(spy).toHaveBeenCalledWith('user:1701:incoming_friend_requests')
     })
+
     test("check the correct arguments have been passed to 'toPusherKey' 2",async ()=>{
         handler.idToAdd ='1984'
         const spy = jest.spyOn(Utils, 'toPusherKey')
         await handler.triggerPusher()
         expect(spy).toHaveBeenCalledWith('user:1984:incoming_friend_requests')
     })
+
     test("confirm idToAdd has been set by the userExists method",async ()=>{
         const expectedID = '1984';
         (fetchRedis as jest.Mock).mockResolvedValue(expectedID);
         await handler.userExists()
         expect(handler.idToAdd).toEqual(expectedID)
     })
+
     test("confirm idToAdd has been set by the userExists method",async ()=>{
         const expectedID = '1489';
         (fetchRedis as jest.Mock).mockResolvedValue(expectedID);
         await handler.userExists()
         expect(handler.idToAdd).toEqual(expectedID)
     })
+
     test("pusher server should get the output of the key as well as the user id and email",async ()=>{
         const spy = jest.spyOn(pusherServer, 'trigger')
         const expected1 ='valid-pusher-key'
@@ -227,16 +240,19 @@ describe("Trigger Pusher tests", ()=>{
         await handler.getSession()
         expect(handler.senderEmail).toEqual(expectedEmail)
     })
-})
+});
 
 describe("IsAlreadyAddedTests", ()=>{
     let handler: PostFriendsRouteHandler
+
     beforeEach(()=>{
         handler = new PostFriendsRouteHandler()
     })
+
     afterEach(()=>{
         jest.resetAllMocks()
     })
+
     test('confirm parameters passed to redis- 1',async ()=>{
         const addId = '1234';
         const senderId = '54321';
@@ -260,12 +276,15 @@ describe("IsAlreadyAddedTests", ()=>{
 
 describe("IsAlreadyAddedTests", ()=> {
     let handler: PostFriendsRouteHandler
+
     beforeEach(() => {
         handler = new PostFriendsRouteHandler()
     })
+
     afterEach(()=>{
         jest.resetAllMocks()
     })
+
     test('confirm parameters passed to redis- 1', async () => {
         const senderId = '54321';
         const addId = '14t63423'
@@ -289,12 +308,15 @@ describe("IsAlreadyAddedTests", ()=> {
 
 describe("addToDB tests",()=>{
     let handler: PostFriendsRouteHandler
+
     beforeEach(()=>{
         handler = new PostFriendsRouteHandler()
     })
+
     afterEach(()=>{
         jest.resetAllMocks()
     })
+
     test('confirm parameters passed to db. sadd',async ()=>{
         const idToAdd='234235'
         const userId = '24325223'
@@ -316,9 +338,11 @@ describe("addToDB tests",()=>{
 
 describe("isSameUserTests",()=>{
     let handler: PostFriendsRouteHandler
+
     beforeEach(()=>{
         handler = new PostFriendsRouteHandler()
     })
+
     test("the user are the same",()=>{
         const idToAdd='235346q3'
         const userId = '235346q3'
@@ -326,6 +350,7 @@ describe("isSameUserTests",()=>{
         handler.senderId = userId
         expect(handler.isSameUser()).toEqual(true)
     })
+
     test("the user are the same",()=>{
         const idToAdd='235346q3'
         const userId = '86576435'
