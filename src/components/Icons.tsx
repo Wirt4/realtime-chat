b9ddf912b5747a4b2fb2fc26bee4eb9bfa854425
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 15:54:35 -0700

    green

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,3 +1,7 @@
+import {LucideProps} from "lucide-react";
+
 export const Icons = {
-    Logo: ()=>{}
+    Logo: (props: LucideProps)=>{
+        return <svg {...props} viewBox='0 0 2000 2000'></svg>
+    }
 }
