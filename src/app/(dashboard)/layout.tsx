f065fde3a83d1924bca06dbe333b772c5edf9703
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 16:22:09 -0700

    red

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ const Layout = async ({children}: LayoutProps)=>{
     return <div className='w-full flex h-screen'>
         <div className='flex h-full w-full max-w-xs frow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'></div>
         <Link href="/dashboard" className='flex h-16 shrink-0 items-center'>
-           <Icons.Logo className='h-8 w-auto text-indigo-600'/>
+           <Icons.Logo className='h-20 w-auto text-indigo-600'/>
         </Link>
         {children}
     </div>;
