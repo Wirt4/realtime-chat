e785af697bba9b5f488ee398818009e66364899d
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-10 14:35:15 -0700

    red

--- __tests__/pages/(dashboard)/layout.test.tsx
+++ __tests__/pages/(dashboard)/layout.test.tsx
@@ -1,2 +1,7 @@
 import '@testing-library/jest-dom'
 import {render, screen, fireEvent} from '@testing-library/react'
+describe('Layout tests',()=>{
+    test('renders without crashing',()=>
+        render(<Layout/>)
+    )
+})
