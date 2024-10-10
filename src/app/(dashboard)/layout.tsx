c823a189201109df4db73943cedf1d469b776aa8
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 16:16:13 -0700

    added logo from icons to page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode} from "react";
 import MyGetServerSession from "@/lib/myGetServerSession";
 import {notFound} from "next/navigation";
 import Link from "next/link";
+import {Icons} from "@/components/Icons";
 
 interface LayoutProps {
     children: ReactNode
@@ -14,7 +15,9 @@ const Layout = async ({children}: LayoutProps)=>{
     }
     return <div className='w-full flex h-screen'>
         <div className='flex h-full w-full max-w-xs frow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'></div>
-        <Link href="/dashboard" className='flex h-16 shrink-0 items-center'></Link>
+        <Link href="/dashboard" className='flex h-16 shrink-0 items-center'>
+           <Icons.Logo className='h-8 w-auto text-indigo-600'/>
+        </Link>
         {children}
     </div>;
 }
