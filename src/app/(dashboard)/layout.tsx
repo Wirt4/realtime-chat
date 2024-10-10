da179c9a0dff841a69403a0d8ca51f6102f2247e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 15:16:47 -0700

    refactor

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,14 @@
 import { ReactNode} from "react";
 import MyGetServerSession from "@/lib/myGetServerSession";
 import {notFound} from "next/navigation";
+
 interface LayoutProps {
     children: ReactNode
 }
-const Layout = async ({children}: LayoutProps)=>{
-    const session = await MyGetServerSession()
-    notFound()
 
+const Layout = async ({children}: LayoutProps)=>{
+    await MyGetServerSession()
+    notFound()
     return <div>{children}</div>;
 }
 
