ab7f3a6ba54b1a257a7bab1c490c0aa9824992bd
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 14:30:06 -0700

    build out layout for dashboard
    remove stray log, some styling
    
    green
    
    red
    
    green
    
    refactor
    
    green, refactored tests after adding div
    
    refactor
    
    green
    
    red
    
    refactor
    
    green
    
    refactor
    
    green
    
    red
    
    green
    
    red
    
    refactor
    
    green
    
    red
    
    stubbed out boilerplate for testing the dashboard layout
    
    created layout for dashboard pages

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode} from "react";
+import MyGetServerSession from "@/lib/myGetServerSession";
+import {notFound} from "next/navigation";
+import Link from "next/link";
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+const Layout = async ({children}: LayoutProps)=>{
+    const session = await MyGetServerSession();
+    if (!session){
+        notFound();
+    }
+    return <div className='w-full flex h-screen'>
+        <div className='flex h-full w-full max-w-xs frow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'></div>
+        <Link href="/dashboard" className='flex h-16 shrink-0 items-center'></Link>
+        {children}
+    </div>;
+}
+
+export default Layout;
