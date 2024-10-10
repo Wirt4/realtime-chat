100d0c2cd86f2a6b7e13bdb903eac14279f38e42
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-09 17:23:25 -0700

    refactor

--- __tests__/helpers/fetchRedis.test.ts
+++ __tests__/helpers/fetchRedis.test.ts
@@ -5,20 +5,24 @@ describe('fetchRedis', () => {
     let originalFetch: any
     let originalToken: any
     let originalUrl: any
+    
     beforeAll(()=>{
         originalFetch = global.fetch
         global.fetch = jest.fn()
         originalToken = process.env.REDIS_TOKEN
         originalUrl = process.env.REDIS_URL
     })
+
     afterEach(()=>{
         jest.resetAllMocks()
     })
+
     afterAll(()=>{
         global.fetch = originalFetch
         process.env.REDIS_TOKEN = originalToken
         process.env.REDIS_URL = originalUrl
     })
+
     test('should fetch redis', async () => {
         const fetchSpy = jest.spyOn(global, 'fetch').mockReturnValue("s'good");
         const command ='sismember'
@@ -27,6 +31,7 @@ describe('fetchRedis', () => {
         await fetchRedis(command, args)
         expect(fetchSpy).toHaveBeenCalledWith('www.sampleurl.com/sismember/somestuff/12345', expect.anything())
     })
+
     test('should fetch redis', async () => {
         const fetchSpy = jest.spyOn(global, 'fetch').mockReturnValue("s'good");
         const args = ['dostuff','54321','63457']
@@ -35,6 +40,7 @@ describe('fetchRedis', () => {
         await fetchRedis(command, args)
         expect(fetchSpy).toHaveBeenCalledWith('www.otherurl.com/zrange/dostuff/54321/63457', expect.anything())
     })
+
     test('should fetch redis, example fot type coercion', async () => {
         const fetchSpy = jest.spyOn(global, 'fetch').mockReturnValue("s'good");
         const args = ['dostuff','54321', 63457]
@@ -43,6 +49,7 @@ describe('fetchRedis', () => {
         await fetchRedis(command, args)
         expect(fetchSpy).toHaveBeenCalledWith('www.otherurl.com/zrange/dostuff/54321/63457', expect.anything())
     })
+
     test('options test, should include bearer token', async () => {
         const fetchSpy = jest.spyOn(global, 'fetch').mockReturnValue("s'good");
         const args = ['dostuff','54321', 63457]
@@ -57,6 +64,7 @@ describe('fetchRedis', () => {
         }
         expect(fetchSpy).toHaveBeenCalledWith(expect.anything(), opts)
     })
+
     test('options test, should include bearer token', async () => {
         const fetchSpy = jest.spyOn(global, 'fetch').mockReturnValue("s'good");
         const args = ['dostuff','54321', 63457]
