5450d11d6ca245cb903bb0006cfbc846d914c78b
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-10 14:53:35 -0700

    red

--- __tests__/pages/(dashboard)/layout.test.tsx
+++ __tests__/pages/(dashboard)/layout.test.tsx
@@ -1,8 +1,13 @@
 import '@testing-library/jest-dom'
-import {render} from '@testing-library/react'
+import {render, screen} from '@testing-library/react';
 import Layout from "@/app/(dashboard)/layout"
+import AddFriendButton from "@/components/AddFriendButton";
 describe('Layout tests',()=>{
     test('renders without crashing',()=>{
         render(<Layout/>);
     });
+    test('renders children',()=>{
+        render(<Layout><AddFriendButton/></Layout>);
+        expect(screen.getByText('Add a Friend by Email:')).toBeInTheDocument();
+    })
 });
