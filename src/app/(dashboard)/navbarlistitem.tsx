b36d83711315a9286ae5aff2887c3c0370881f9b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 17:56:59 -0700

    green

--- src/app/(dashboard)/navbarlistitem.tsx
+++ src/app/(dashboard)/navbarlistitem.tsx
@@ -1,10 +1,13 @@
 'use client'
 import {FC} from "react";
+import Link from "next/link";
 
 interface navbarListItemPrps {}
 
 const NavbarListItem: FC<navbarListItemPrps> =  ({}) =>{
-    return <li></li>
+    return <li>
+        <Link href='stub'>stub</Link>
+    </li>
 }
 
 export default NavbarListItem
