e7b3b5d59b9730dcb6e9f5860793cd54076a3c59
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 18:20:20 -0700

    green

--- src/app/(dashboard)/navbarlistitem.tsx
+++ src/app/(dashboard)/navbarlistitem.tsx
@@ -1,14 +1,16 @@
 'use client'
 import {FC} from "react";
 import Link from "next/link";
+import {Icons} from "@/components/Icons";
 
 interface navbarListItemPrps {
     href: string;
 }
 
 const NavbarListItem: FC<navbarListItemPrps> =  (props) =>{
+    const Icon = Icons.Logo
     return <li>
-        <Link href={props.href}>stub</Link>
+        <Link href={props.href}><span><Icon/></span></Link>
     </li>
 }
 
