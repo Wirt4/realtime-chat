666359ca5871f531319b5b82054f641bc1142570
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 18:01:55 -0700

    refactor

--- __tests__/pages/(dashboard)/navBarListItem.test.tsx
+++ __tests__/pages/(dashboard)/navBarListItem.test.tsx
@@ -2,14 +2,15 @@ import '@testing-library/jest-dom'
 import NavbarListItem from "@/app/(dashboard)/navbarlistitem"
 import {render, screen} from "@testing-library/react";
 describe('NavBarListItem', () => {
-    test('make sure component as list item',()=>{
+    beforeEach(()=>{
         render(<NavbarListItem />);
+    })
+    test('make sure component as list item',()=>{
         const listItem = screen.getByRole('listitem');
         expect(listItem).toBeInTheDocument();
     });
 
     test('make sure component contains Link',()=>{
-        render(<NavbarListItem />);
         const link = screen.getByRole('link')
         expect(link).toBeInTheDocument();
     });
