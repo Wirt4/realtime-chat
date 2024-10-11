2609b444c5de57f57037b7f797390fd302eecf43
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 17:42:13 -0700

    green

---
+++ src/app/(dashboard)/navbarlistitem.tsx
@@ -0,0 +1,10 @@
+'use client'
+import {FC} from "react";
+
+interface navbarListItemPrps {}
+
+const NavbarListItem: FC<navbarListItemPrps> =  ({}) =>{
+    return <></>
+}
+
+export default NavbarListItem
