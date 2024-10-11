10efe9e4a8921cd417881fc5d8bb262747fc0159
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-10 17:40:29 -0700

    red

--- __tests__/pages/(dashboard)/navBarListItem.test.tsx
+++ __tests__/pages/(dashboard)/navBarListItem.test.tsx
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom'
+import NavbarListItem from "@/app/(dashboard)/navbarlistitem"
+import {render} from "@testing-library/react";
+describe('NavBarListItem', () => {
+    test('make sure component renders',()=>{
+        render(<NavbarListItem />)
+    })
+})
