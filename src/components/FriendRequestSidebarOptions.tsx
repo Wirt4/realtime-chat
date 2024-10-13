5e778c592c321bbb9148a64e15523ecabfc95153
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 21:01:21 -0700

    green

--- src/components/FriendRequestSidebarOptions.tsx
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import {Icons} from "@/components/Icons";
 const User = Icons['User']
 const FriendRequestSidebarOptions: FC = ()=>{
-    return <><Link href='/dashboard/requests'><div>
-        <User aria-label='User'/>
-    </div></Link></>;
+    return <><Link href='/dashboard/requests'>
+        <div>
+            <User aria-label="User"/>
+            <p>Friend Requests</p>
+        </div>
+    </Link></>;
 }
 
 export default FriendRequestSidebarOptions;
