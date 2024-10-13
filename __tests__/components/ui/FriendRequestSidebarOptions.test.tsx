68d96f6fdf023a62ede22aa9557c41a6c44141f9
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 20:20:03 -0700

    red

---
+++ __tests__/components/ui/FriendRequestSidebarOptions.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom'
+import {render} from '@testing-library/react'
+
+describe('FriendRequestSidebarOptions', () => {
+    test('Component renders without error', ()=>{
+        render(<FriendRequestSidebarOptions/>)
+    })
+})
