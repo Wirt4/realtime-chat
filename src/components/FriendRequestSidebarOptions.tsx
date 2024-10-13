37a388b9d139a09c63cf73fb16d8be06b65b60f5
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 20:54:05 -0700

    green

--- src/components/FriendRequestSidebarOptions.tsx
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -1,9 +1,12 @@
 'use client';
 import {FC} from "react";
 import Link from "next/link";
-
+import {Icons} from "@/components/Icons";
+const User = Icons['User']
 const FriendRequestSidebarOptions: FC = ()=>{
-    return <><Link href='/dashboard/requests'>hi</Link></>;
+    return <><Link href='/dashboard/requests'><div>
+        <User aria-label='User'/>
+    </div></Link></>;
 }
 
 export default FriendRequestSidebarOptions;
