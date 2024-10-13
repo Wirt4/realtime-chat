34495474e06f49b318068675b6859dc9b86635ca
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 20:32:14 -0700

    green

--- src/components/FriendRequestSidebarOptions.tsx
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -3,7 +3,7 @@ import {FC} from "react";
 import Link from "next/link";
 
 const FriendRequestSidebarOptions: FC = ()=>{
-    return <><Link href='stub'>hi</Link></>;
+    return <><Link href='/dashboard/requests'>hi</Link></>;
 }
 
 export default FriendRequestSidebarOptions;
