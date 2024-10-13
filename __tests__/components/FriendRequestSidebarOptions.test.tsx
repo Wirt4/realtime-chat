31647287ddf30c09d16dcdf2b5e8325c9ede6704
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 20:31:17 -0700

    red

--- __tests__/components/FriendRequestSidebarOptions.test.tsx
+++ __tests__/components/FriendRequestSidebarOptions.test.tsx
@@ -11,4 +11,10 @@ describe('FriendRequestSidebarOptions', () => {
         const buttonElement = screen.getByRole('link');
         expect(buttonElement).toBeInTheDocument();
     });
+    test('Component contains a link', ()=>{
+        render(<FriendRequestSidebarOptions/>);
+        const linkElement = screen.getByRole('link');
+        expect(linkElement).toBeInTheDocument();
+        expect(linkElement).toHaveAttribute('href', '/dashboard/requests');
+    });
 });
