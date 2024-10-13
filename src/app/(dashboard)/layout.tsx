f82f41c7fed8c7c9d19e00eecb668a5948b4cca2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 22:10:36 -0700

    refactor

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -15,6 +15,7 @@ const Layout = async ({children}: LayoutProps)=>{
     if (!session){
         notFound();
     }
+
     return <div className='w-full flex h-screen'>
         <div className='flex h-full max-w-xs frow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white pt-6 px-6'>
         <Link href="/dashboard" className='flex h-16 shrink-0 items-center'>
