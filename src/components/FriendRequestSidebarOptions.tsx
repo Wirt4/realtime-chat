ca7bcb92d862dae049ae422d73401cec38e7c5be
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 21:11:01 -0700

    refactor

--- src/components/FriendRequestSidebarOptions.tsx
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -4,19 +4,17 @@ import Link from "next/link";
 import {Icons} from "@/components/Icons";
 const User = Icons['User']
 
-interface FriendRequestSidebarOptionsProps{
-    initialUnseenRequestCount: number
+interface FRSOProps{
+    requestCount: number
 }
 
-const FriendRequestSidebarOptions: FC<FriendRequestSidebarOptionsProps> = ({
-                                                                               initialUnseenRequestCount
-                                                                           })=>{
+const FriendRequestSidebarOptions: FC<FRSOProps> = ({requestCount})=>{
     return <>
         <Link href='/dashboard/requests'>
         <div>
             <User aria-label="User"/>
             <p>Friend Requests</p>
-            <div>{initialUnseenRequestCount}</div>
+            <div>{requestCount}</div>
         </div>
     </Link>
     </>;
