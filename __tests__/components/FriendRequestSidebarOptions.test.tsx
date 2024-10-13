1c2fdb39edeb49aa7b9bf8b042eef3caff9542cb
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 20:25:52 -0700

    red

--- __tests__/components/FriendRequestSidebarOptions.test.tsx
+++ __tests__/components/FriendRequestSidebarOptions.test.tsx
@@ -1,9 +1,14 @@
 import '@testing-library/jest-dom';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOptions";
 
 describe('FriendRequestSidebarOptions', () => {
     test('Component renders without error', ()=>{
         render(<FriendRequestSidebarOptions/>);
+    });
+    test('Component contains a link', ()=>{
+        render(<FriendRequestSidebarOptions/>);
+        const buttonElement = screen.getByRole('link');
+        expect(buttonElement).toBeInTheDocument();
     });
 });
