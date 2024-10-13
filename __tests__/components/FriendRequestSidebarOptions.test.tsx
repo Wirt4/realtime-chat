6fe19ee5b437cc43bd571eb124cd8275f45f8050
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 21:00:08 -0700

    red

--- __tests__/components/FriendRequestSidebarOptions.test.tsx
+++ __tests__/components/FriendRequestSidebarOptions.test.tsx
@@ -25,4 +25,9 @@ describe('FriendRequestSidebarOptions', () => {
         const name = screen.getByLabelText('User');
         expect(name).toBeInTheDocument();
     });
+    test('Component should contain text "Friend Requests"',()=>{
+        render(<FriendRequestSidebarOptions/>);
+        const label = screen.getByText("Friend Requests");
+        expect(label).toBeInTheDocument();
+    });
 });
