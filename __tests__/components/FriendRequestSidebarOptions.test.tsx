e54661119c38fa8d99df96e60580adc264c856d9
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-12 20:23:00 -0700

    green

--- __tests__/components/FriendRequestSidebarOptions.test.tsx
+++ __tests__/components/FriendRequestSidebarOptions.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import {render} from '@testing-library/react'
+import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOptions";
 
 describe('FriendRequestSidebarOptions', () => {
     test('Component renders without error', ()=>{
---
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -0,0 +1,8 @@
+'use client'
+import {FC} from "react";
+
+const FriendRequestSidebarOptions: FC = ()=>{
+    return <></>
+}
+
+export default FriendRequestSidebarOptions;
