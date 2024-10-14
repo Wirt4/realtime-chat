31f694f1b13aa2a769593bc271c3e400f50bbefa
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-13 21:42:23 -0700

    spacing and presently unnecessary work

--- __tests__/components/FriendRequestSidebarOptions/FriendRequestEffect.test.ts
+++
@@ -1,8 +0,0 @@
-import FriendRequestEffect from "@/components/friendRequestSidebarOptions/FriendRequestEffect";
-describe('FriendRequestEffect tests, test the anonymous function returned', () => {
-    test('expect pusherClient.subscribe to be called for user__1701__incoming_friend_requests',()=>{
-        const anon = FriendRequestEffect()
-        anon()
-
-    })
-});
