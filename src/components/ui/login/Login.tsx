72f835acc008d57a5f375e4b0bd5900629dbd6f2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-13 23:54:16 -0700

    finish migration

--- src/components/ui/login/Login.tsx
+++ src/components/ui/login/Login.tsx
@@ -9,25 +9,22 @@ import {Icons} from "@/components/Icons";
 const Login:FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const Logo = Icons.Logo
-    return <>
-        <div className= 'flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-            <div className='w-full flex flex-col items-center max-w-md space-y-8'>
-                <div className='flex flex-col items-center gap-0'>
-                    <Logo/>
-                    <h2 className='mt-0 text-center text-3xl font-bold tracking-tight text-gray-900'>
+    return <div className= 'login-page'>
+            <div className='login-body'>
+                <div>
+                    <Logo className='login-logo'/>
+                    <h2 className='login-h2'>
                         Sign in with Google
                     </h2>
                 </div>
-                <Button isLoading={isLoading} type='button'
-                        className='max-w-sm mx-auto w-full'
-                        onClick={Utils.loginWithGoogle(setIsLoading)}
-                >
-                    {isLoading? null: <GoogleLogo/>}
-                    Google
-                </Button>
+                    <Button isLoading={isLoading} type='button'
+                        className='login-button'
+                        onClick={Utils.loginWithGoogle(setIsLoading)}>
+                            {isLoading? null: <GoogleLogo/>}
+                            Google
+                    </Button>
             </div>
         </div>
-    </>
 }
 
 export default Login
