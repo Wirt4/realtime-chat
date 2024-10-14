ef6bc41f9875f7dd8a4529163d42e29ffa50575c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-13 22:29:00 -0700

    moved navbarlist styles to css

--- src/app/(dashboard)/navbarlistitem.tsx
+++ src/app/(dashboard)/navbarlistitem.tsx
@@ -2,7 +2,6 @@
 import {FC} from "react";
 import Link from "next/link";
 import {Icon, Icons} from "@/components/Icons";
-import navBarListItemClassNames from "@/app/(dashboard)/navBarListItemClassNames";
 
 export interface navbarListItemProps {
     href: string;
@@ -15,11 +14,11 @@ const NavbarListItem: FC<navbarListItemProps> =  (props) =>{
     const Icon = Icons[props.Icon]
     return <li>
         <Link href={props.href} className='link group'>
-            <span className={navBarListItemClassNames.Link.span.className}>
-                <Icon className='icon-1' aria-label={props.Icon as string}/>
+            <span className='link-icon group-hover:border-indigo-600 group-hover:text-indigo-600'>
+                <Icon className='icon' aria-label={props.Icon as string}/>
             </span>
+        <span className='truncate'>{props.name}</span>
         </Link>
-        <span className={navBarListItemClassNames.span.className}>{props.name}</span>
     </li>
 }
 
