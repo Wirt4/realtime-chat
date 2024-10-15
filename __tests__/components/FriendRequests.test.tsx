12a73221095ff1c72d3fed9b4db43304d5343476
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-15 16:01:37 -0700

    red

--- __tests__/components/FriendRequests.test.tsx
+++ __tests__/components/FriendRequests.test.tsx
@@ -25,5 +25,13 @@ describe('FriendRequests', () => {
         render(<FriendRequests incomingFriendRequests={requests}/>);
         const text = screen.queryByText('Nothing to show here...');
         expect(text).not.toBeInTheDocument();
+    });
+    test('sender emails should be listed',()=>{
+        const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}, {senderId: 'bar', senderEmail: 'bar@foo.com'}]
+        render(<FriendRequests incomingFriendRequests={requests}/>);
+        const button1 = screen.queryByText('foo@bar.com');
+        const button2 = screen.queryByText('bar@foo.com');
+        expect(button1).toBeInTheDocument();
+        expect(button2).toBeInTheDocument();
     })
 });
