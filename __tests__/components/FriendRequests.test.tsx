17d38c812867f90420dfaada1f35131e312e718b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-15 15:12:06 -0700

    red

--- __tests__/components/FriendRequests.test.tsx
+++ __tests__/components/FriendRequests.test.tsx
@@ -3,9 +3,9 @@ import FriendRequests from "@/components/FriendRequests";
 import {render, screen} from "@testing-library/react";
 
 describe('FriendRequests', () => {
-    test('Should render a div that reads "FriendRequests',()=>{
+    test('final state of friend requests is 0, should render "Nothing to show here..." ',()=>{
         render(<FriendRequests />);
-        const text = screen.getByText('FriendRequests');
+        const text = screen.getByText('Nothing to show here...');
         expect(text).toBeInTheDocument();
     });
 });
