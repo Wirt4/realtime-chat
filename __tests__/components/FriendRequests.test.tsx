e4f63241ade95a14591ffd96aad1735c1950a6fc
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-15 16:26:01 -0700

    refactor

--- __tests__/components/FriendRequests.test.tsx
+++ __tests__/components/FriendRequests.test.tsx
@@ -8,24 +8,28 @@ describe('FriendRequests', () => {
         const text = screen.getByText('Nothing to show here...');
         expect(text).toBeInTheDocument();
     });
+
     test('if the component receives a list of length 2, then there should be two UserPlus icons in the document',()=>{
         const requests = [{senderId:'foo', senderEmail: 'bar'}, {senderId: 'bar', senderEmail: 'foo'}]
         render(<FriendRequests incomingFriendRequests={requests}/>);
         const icons = screen.getAllByLabelText('add user');
         expect(icons).toHaveLength(2);
     });
+
     test('if the component receives a list of length 2, then there should be two UserPlus icons in the document',()=>{
         const requests = [{senderId:'foo', senderEmail: 'bar'}]
         render(<FriendRequests incomingFriendRequests={requests} />);
         const icons = screen.getAllByLabelText('add user');
         expect(icons).toHaveLength(1);
     });
+
     test('if (final) requests are greater than 0, do not render "Nothing to show here...',()=>{
         const requests = [{senderId:'foo', senderEmail: 'bar'}, {senderId: 'bar', senderEmail: 'foo'}]
         render(<FriendRequests incomingFriendRequests={requests}/>);
         const text = screen.queryByText('Nothing to show here...');
         expect(text).not.toBeInTheDocument();
     });
+
     test('sender emails should be listed',()=>{
         const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}, {senderId: 'bar', senderEmail: 'bar@foo.com'}]
         render(<FriendRequests incomingFriendRequests={requests}/>);
@@ -34,18 +38,21 @@ describe('FriendRequests', () => {
         expect(button1).toBeInTheDocument();
         expect(button2).toBeInTheDocument();
     });
+
     test('if the component receives a list of length 2, then there should be two elements with the label "accept friend"',()=>{
         const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}, {senderId: 'bar', senderEmail: 'bar@foo.com'}]
         render(<FriendRequests incomingFriendRequests={requests} />);
         const buttons = screen.getAllByLabelText('accept friend');
         expect(buttons).toHaveLength(2);
     });
+
     test('elements with the label "accept friend" should be a button',()=>{
         const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}]
         render(<FriendRequests incomingFriendRequests={requests} />);
         const button = screen.getByLabelText('accept friend');
         expect(button.tagName).toBe('BUTTON');
     });
+
     test('accept friend should contain a checkmark',()=>{
         const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}]
         render(<FriendRequests incomingFriendRequests={requests} />);
@@ -53,12 +60,15 @@ describe('FriendRequests', () => {
         const check = within(button).getByLabelText('checkmark');
         expect(check).toBeInTheDocument();
     });
-    test('if the component receives a list of length 2, then there should be two elements with the label "deny friend"',()=>{
+
+    test('if the component receives a list of length 2, then there should be two elements with the label "deny friend"',
+        ()=>{
         const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}, {senderId: 'bar', senderEmail: 'bar@foo.com'}]
         render(<FriendRequests incomingFriendRequests={requests} />);
         const buttons = screen.getAllByLabelText('deny friend');
         expect(buttons).toHaveLength(2);
     });
+
     test('elements with the label "deny friend" should be a button',()=>{
         const requests = [{senderId:'foo', senderEmail: 'foo@bar.com'}]
         render(<FriendRequests incomingFriendRequests={requests} />);
