9eac0def753d12b6cbb453f47c56461607118064
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-17 16:43:48 -0700

    remove stub function

--- src/lib/pusher.ts
+++
@@ -1,9 +0,0 @@
-//want to know what pusher and pusher-js do before implementing them blindly
-
-export const pusherClient = {
-    //stub
-}
-export const pusherServer =  {
-    //stub
-    trigger:(foo: never, bar: never, bee: never)=>{}
-}
