84154aa609247e853f0a8c99f6ac8c29f7512491
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-16 23:11:12 -0700

    build out endpoint

--- __tests__/api/friends/accept/route.test.ts
+++ __tests__/api/friends/accept/route.test.ts
@@ -1,18 +1,211 @@
 import { POST } from "@/app/api/friends/accept/route";
+import myGetServerSession from "@/lib/myGetServerSession";
+import fetchRedis from "@/helpers/redis";
+import { db } from '@/lib/db';
+
+jest.mock("../../../../src/lib/myGetServerSession",()=>({
+    __esModule: true,
+    default: jest.fn()
+}));
+
+jest.mock("../../../../src/lib/db",()=>({
+    __esModule: true,
+    db: {
+        sadd: jest.fn(),
+        srem: jest.fn()
+    }
+}));
+
+jest.mock('../../../../src/helpers/redis')
+
+interface expectedResponse {
+    text: string,
+    status: number
+}
 
 describe('/api/friends/accept', () => {
     beforeEach(() => {
-        fetchMock.resetMocks(); // Ensure fresh mocks
+        (fetchRedis as jest.Mock).mockResolvedValue(false);
     });
+
+    afterEach(()=>{
+        jest.resetAllMocks();
+    })
 
     test('POST should run without throwing', async () => {
         fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
-
-        const req = new Request('http://localhost/api/friends/accept', {
-            method: 'POST',
-            body: JSON.stringify({ someData: 'value' }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+        const req = requestify('valid')
         await POST(req);
     });
+
+    test('method throws if value is not string', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        const req = requestify(42)
+        const expectedResponse: expectedResponse = {
+            status: 422,
+            text: 'Invalid Request'
+        }
+        const response = await POST(req);
+        assertResponse(response, expectedResponse)
+    });
+
+    test('If session is null, return 401 UnAuthorized', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue(null);
+        const req = requestify('valid')
+        const expected: expectedResponse = {
+            status:401,
+            text: 'Unauthorized'
+        }
+        const response =  await POST(req);
+        assertResponse(response, expected);
+    });
+
+    test('If session is not null, do not return 401 UnAuthorized', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1701'}});
+        const req = requestify('valid')
+        const response =  await POST(req);
+        const notExpected: expectedResponse = {
+            status: 401,
+            text: 'Unauthorized'
+        }
+        assertNotResponse(response, notExpected);
+    });
+
+    test('fetchRedis(sismember, user:1701:friends is truthy, response should be Already Friends 400', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1701'}});
+        const req = requestify('valid');
+        (fetchRedis as jest.Mock).mockResolvedValue(true);
+
+        const response =  await POST(req);
+        const expected: expectedResponse = {
+            text: 'Already Friends',
+            status: 400
+        }
+        assertResponse(response, expected)
+    });
+
+    test('fetchRedis(sismember, user:1701:friends is falsy, response should not be Already Friends 400', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1701'}});
+        const req = requestify('valid');
+        (fetchRedis as jest.Mock).mockResolvedValue(false);
+
+        const response =  await POST(req);
+        const expected: expectedResponse = {
+            text: 'Already Friends',
+            status: 400
+        }
+        assertNotResponse(response, expected)
+    });
+
+    test('fetchRedis should be called with sismember, user:1701:friends, 5468  ', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1701'}});
+        const req = requestify('5468');
+        await POST(req);
+        expect(fetchRedis as jest.Mock).toHaveBeenCalledWith('sismember','user:1701:friends', '5468');
+    });
+
+    test('fetchRedis should be called with sismember, user:1966:friends, 1701 ', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1966'}});
+        const req = requestify('1701');
+        await POST(req);
+        expect(fetchRedis as jest.Mock).toHaveBeenCalledWith('sismember','user:1966:friends', '1701');
+    });
+
+    test("fetcRedis' call to :incoming_friend_requests returns a falsy value", async()=>{
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1966'}});
+        (fetchRedis as jest.Mock).mockImplementation(redisMock(false))
+        const expected: expectedResponse ={
+            text:'No Existing Friend Request',
+            status: 400
+        }
+
+        const request = requestify('1701');
+        const response = await POST(request);
+
+        assertResponse(response, expected);
+    });
+
+    test("fetcRedis' call to :incoming_friend_requests returns a truthy value, expect db.sadd to be called", async()=>{
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'1966'}});
+        (fetchRedis as jest.Mock).mockImplementation(redisMock())
+
+        const request = requestify('1701');
+        await POST(request);
+        expect(db.sadd).toHaveBeenCalledWith('user:1966:friends', '1701');
+        expect(db.sadd).toHaveBeenCalledWith('user:1701:friends', '1966');
+    });
+
+    test("fetcRedis' call to :incoming_friend_requests returns a truthy value, expect db.sadd to be called, different data", async()=>{
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'8569'}});
+        (fetchRedis as jest.Mock).mockImplementation(redisMock());
+
+        const request = requestify('666');
+        await POST(request);
+
+        expect(db.sadd).toHaveBeenCalledWith('user:8569:friends', '666');
+        expect(db.sadd).toHaveBeenCalledWith('user:666:friends', '8569');
+    });
+
+    test("expect the idtoAdd to be removed from the user's requests table", async()=>{
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'8569'}});
+        (fetchRedis as jest.Mock).mockImplementation(redisMock());
+
+        const request = requestify('666');
+        await POST(request);
+
+        expect(db.srem).toHaveBeenCalledWith('user:8569:incoming_friend_requests', '666');
+    });
+
+    test("expect the idtoAdd to be removed from the user's requests table", async()=>{
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+        (myGetServerSession as jest.Mock).mockResolvedValue({user:{id:'4567'}});
+        (fetchRedis as jest.Mock).mockImplementation(redisMock());
+
+        const request = requestify('7777');
+        await POST(request);
+
+        expect(db.srem).toHaveBeenCalledWith('user:4567:incoming_friend_requests', '7777');
+    });
 });
+
+function assertResponse( response: Response, expected: expectedResponse): void{
+    expect(response.status).toEqual(expected.status);
+    expect(response.body?.toString()).toEqual(expected.text);
+}
+
+function assertNotResponse( response: Response, expected: expectedResponse): void{
+    expect(response.status === expected.status && response.body?.toString()== expected.text);
+}
+
+function requestify(id: string | number): Request{
+    return new Request('/api/friends/accept', {
+        method: 'POST',
+        body: JSON.stringify({ id: id }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
+
+function redisMock(isGreen: boolean = true){
+    return async ( command: string, query:string, opts:string)=>{
+        const arr = query.split(':');
+        const table_name = arr[arr.length-1];
+        switch(table_name){
+            case 'friends':
+                return false;
+            case 'incoming_friend_requests':
+                return isGreen ? 1 : 0;
+            default:
+                throw new Error('invalid table');
+        }
+    }
+}
--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,3 +1,98 @@
-export async function  POST(req: Request):Promise<Response> {
-    throw new Error('Not implemented');
+import {z} from 'zod'
+import myGetServerSession from "@/lib/myGetServerSession";
+import fetchRedis from "@/helpers/redis";
+import {db} from "@/lib/db";
+
+export async function POST(request: Request):Promise<Response> {
+    const idToAdd = await getIdToAdd(request);
+    if (!idToAdd) {
+        return respond('Invalid Request', 422);
+    }
+
+    const  userId = await getUserId();
+    if (!userId) {
+        return respond('Unauthorized', 401);
+    }
+
+    const handler = new Handler(idToAdd as string, userId as string);
+    const areAlreadyFriends: boolean = await handler.areFriends();
+
+    if (areAlreadyFriends) {
+        return respond('Already Friends', 400);
+    }
+
+    const hasExistingFriendRequest = await handler.isInRequests();
+
+    if (!hasExistingFriendRequest) {
+        return respond('No Existing Friend Request', 400);
+    }
+
+    await handler.addtoFriendsTabeles();
+    await handler.removeRequestFromTable();
+    return new Response('OK');
+}
+
+class Handler{
+    idToAdd: string;
+    userId: string;
+
+    constructor(idToAdd: string, userId: string) {
+        this.idToAdd = idToAdd;
+        this.userId = userId;
+    }
+
+    async areFriends(): Promise<boolean>{
+        return fetchRedis('sismember', this.friendsTable(), this.idToAdd);
+    }
+
+    async isInRequests(): Promise<boolean>{
+       return  await fetchRedis('sismember', this.incomingRequestsQuery(), this.idToAdd) as number == 1;
+    }
+
+    async addtoFriendsTabeles():Promise<void>{
+        await this.addToFriendsTable(this.userId, this.idToAdd);
+        await this.addToFriendsTable(this.idToAdd, this.userId);
+    }
+
+    async removeRequestFromTable():Promise<void>{
+        await db.srem(this.incomingRequestsQuery(), this.idToAdd);
+    }
+
+    friendsTable(id: string = this.userId): string {
+        return this.userTable(id, 'friends');
+    }
+
+    incomingRequestsQuery(): string {
+        return this.userTable(this.userId, 'incoming_friend_requests');
+    }
+
+    async addToFriendsTable(userId: string = this.userId, idToAdd: string = this.idToAdd):Promise<void>{
+        await db.sadd(this.friendsTable(userId), idToAdd);
+    }
+
+    userTable(id: string, suffix: 'friends'| 'incoming_friend_requests'){
+        return `user:${id}:${suffix}`;
+    }
+}
+
+
+function respond(text: string, status: number): Response
+{
+    return  new Response(text, {status: status});
+}
+
+async function getUserId(): Promise<string | boolean>{
+    const session = await myGetServerSession();
+    return session?.user?.id || false;
+}
+
+async function getIdToAdd(request: Request):Promise<string | boolean> {
+    const body = await request.json();
+    try{
+        const { id: idToAdd } =  z.object({id: z.string()}).parse(body);
+        return idToAdd;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    }catch(error){
+        return false;
+    }
 }
