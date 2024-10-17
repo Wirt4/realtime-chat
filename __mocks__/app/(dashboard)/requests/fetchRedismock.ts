393ea4c4f15280ee4134417240c23b28650dc118
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-16 17:22:41 -0700

    updated mock

--- __mocks__/app/(dashboard)/requests/fetchRedismock.ts
+++ __mocks__/app/(dashboard)/requests/fetchRedismock.ts
@@ -1,49 +1,62 @@
-const fetchRedisMock1 = async(cmd:string, query:string)=>{
-    if (cmd ==='smembers'){
-        return['k1234','k5678', 'k90123' ];
+const users1: {[index: string]:User}  = {
+    'user:k1234': {
+        name: "Santino",
+        email: 'sonny@correlone.edu',
+        image: 'stub',
+        id:'k1234'
+    },
+    'user:k5678':{
+        name: "Freddie the Fisher",
+        email: 'fredo@correlone.edu',
+        image: 'stub',
+        id:'k5678'
+    },
+    'user:k90123': {
+        name: "Michael",
+        email: 'michael@correlone.edu',
+        image: 'stub',
+        id:'k90123'
     }
-    const users: {[index: string]:User}  = {
-        'user:k1234': {
-            name: "Santino",
-            email: 'sonny@correlone.edu',
-            image: 'stub',
-            id:'k1234'
-        },
-        'user:k5678':{
-            name: "Freddie the Fisher",
-            email: 'fredo@correlone.edu',
-            image: 'stub',
-            id:'k5678'
-        },
-        'user:k90123':{
-            name: "Michael",
-            email: 'michael@correlone.edu',
-            image: 'stub',
-            id:'k90123'
-        }
-    }
-    return JSON.stringify(users[query]);
 }
 
-const fetchRedisMock2 = async(cmd:string, query:string)=>{
-    if (cmd ==='smembers'){
-        return['l1234','l5678' ]
+const  users2 : {[index: string]:User}  = {
+    'user:l1234': {
+        name: "Clemenza",
+        email: 'clemenza@correlone.edu',
+        image: 'stub',
+        id:'l1234'
+    },
+    'user:l5678':{
+        name: "Tessio",
+        email: 'tessio@correlone.edu',
+        image: 'stub',
+        id:'l5678'
     }
-    const users: {[index: string]:User}  = {
-        'user:l1234': {
-            name: "Clemenza",
-            email: 'clemenza@correlone.edu',
-            image: 'stub',
-            id:'l1234'
-        },
-        'user:l5678':{
-            name: "Tessio",
-            email: 'tessio@correlone.edu',
-            image: 'stub',
-            id:'l5678'
-        }
+}
+
+
+interface mockProps {
+    command: Command,
+    query: string
+}
+
+const mockUsers = (props: mockProps, users: {[index: string]:User} )=>{
+    if (props.command === 'smembers'){
+        const keys  = Object.keys(users) as string[];
+        return  keys.map(key=>{
+            return key.split(':')[1];
+        });
     }
-    return JSON.stringify(users[query])
+
+    return JSON.stringify(users[props.query]);
+}
+
+const fetchRedisMock1 = async(command:Command, query:string)=> {
+    return mockUsers({command, query}, users1);
+}
+
+const fetchRedisMock2 = async(command:Command, query:string)=> {
+    return mockUsers({command, query}, users2);
 }
 
 export{fetchRedisMock1, fetchRedisMock2};
