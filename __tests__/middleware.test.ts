2b416ac24db8c76dbb2a9df0511ef9a8194a528e
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-17 17:10:08 -0700

    green

--- __tests__/middleware.test.ts
+++ __tests__/middleware.test.ts
@@ -1,3 +1,6 @@
-describe('Middleware', () => {
-    test('stub', () => {});
+import {config} from "@/middleware"
+describe('Middleware, config tests', () => {
+    test('config needs a machers key', () => {
+        expect(config).toEqual(expect.objectContaining({matchers: expect.anything()}))
+    });
 });
---
+++ src/middleware.ts
@@ -0,0 +1,1 @@
+export const config = {matchers: 'null'}
