24e561ea3601050161b03ada5f8994bb3857831b
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-19 13:59:18 -0500

    separate click handling implementation from inline

--- src/components/signOutButton.tsx
+++ src/components/signOutButton.tsx
@@ -8,10 +8,8 @@ import {toast} from "react-hot-toast";
 
 interface SignOutProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const SignOutButton: FC<SignOutProps> = ({...props}) => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    return <Button {...props} variant='ghost' onClick={async () => {
+const clickHandler = (setIsLoading: (arg: boolean)=> void) =>{
+    return async()=>{
         try{
             setIsLoading(true);
             await signOut();
@@ -21,7 +19,14 @@ const SignOutButton: FC<SignOutProps> = ({...props}) => {
         }finally{
             setIsLoading(false)
         }
-    }} aria-label= 'sign out button' className='dashboard-icon'>
+    }
+}
+
+const SignOutButton: FC<SignOutProps> = ({...props}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    return <Button {...props} variant='ghost' onClick={clickHandler(setIsLoading)}
+                   aria-label= 'sign out button' className='dashboard-icon'>
         <div className='dashboard-div'>
             {isLoading? (<Loader2 className='loading' aria-label="loading"/>):(<LogOut className='icon' aria-label="log out"/>)}
         </div>
