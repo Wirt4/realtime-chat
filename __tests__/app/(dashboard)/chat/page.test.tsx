83fe9e514bd2597753e0a40be73a24e4d34391ca
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-10-20 00:04:27 -0500

    red

--- __tests__/app/(dashboard)/chat/page.test.tsx
+++ __tests__/app/(dashboard)/chat/page.test.tsx
@@ -1,3 +1,9 @@
+import '@testing-library/jest-dom'
+import {render} from '@testing-library/react'
+import Page from '@/app/(dashboard)/chat/page'
+
 describe('ChatPage tests', () => {
-    test('stub',()=>{});
+    test('page renders',()=>{
+        render(<Page/>)
+    });
 });
