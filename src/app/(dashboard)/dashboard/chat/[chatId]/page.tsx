79303e836b94b1ee1f3e77138cdf790eea4bde72
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-22 23:31:13 -0500

    refactor

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -16,7 +16,17 @@ const Page: FC<ChatProps> = async ({params}) => {
     }
     const imageSource ="https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600,c_limit/Ideas_Surprised_Pikachu_HD.jpg"
 
-    return <div><Image src={imageSource} width="40" height="40" alt={'pikachu'}/></div>
+    return<div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+        <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+            <div className='relative flex items-center space-x-4'>
+                <div className='relative'>
+                    <div className='relative w-8 sm:w-12 sm:h-12'>
+                        <Image src={imageSource} fill alt={'pikachu'} referrerPolicy='no-referrer'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 
