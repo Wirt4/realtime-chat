82b994868a4f2fa6d78b0b1717af096479201981
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-23 21:59:28 -0500

    refactor

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -3,6 +3,6 @@
 import {FC} from "react";
 
 const Messages: FC = () => {
-    return <div aria-label='messages'/>
+    return <div aria-label='messages' className='message-scroll'/>
 }
 export default Messages;
