0385f6aa91c7ec6951ca2ed4a2f11fc4f49f8fb1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-28 10:39:28 -0700

    refactor

--- __tests__/lib/queryBuilder.test.ts
+++ __tests__/lib/queryBuilder.test.ts
@@ -2,10 +2,10 @@ import QueryBuilder from "@/lib/queryBuilder";
 
 describe('QueryBuilder Test', () => {
     test('Should return user:{id}', () => {
-        expect(QueryBuilder.user('1234')).toEqual('user:1234')
-    })
+        expect(QueryBuilder.user('1234')).toEqual('user:1234');
+    });
 
     test('Should return user:{id}, different data', () => {
-        expect(QueryBuilder.user('frank')).toEqual('user:frank')
-    })
-})
+        expect(QueryBuilder.user('frank')).toEqual('user:frank');
+    });
+});
