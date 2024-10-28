b7537d9df545c79c1626d45dbb14d86d1ca771a3
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-28 15:46:49 -0700

    installed nanoid

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -6,8 +6,10 @@ import QueryBuilder from "@/lib/queryBuilder";
 export async function POST(request: Request) {
     const session = await myGetServerSession()
     const {chatId} = await request.json()
+
     const chatParticipants = new Participants(chatId, session?.user?.id as string )
     const query = QueryBuilder.friends(session?.user.id as string )
+
     const friendList = await fetchRedis('smembers', query) as string[]
 
     if (!chatParticipants.includesSession() || !friendList.includes(chatParticipants.partnerId())){
