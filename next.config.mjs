02c904a87afd8e8803075b95eba8d0a5018d2cd1
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-28 10:21:43 -0700

    add comment to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        /** user images are assumed to be sourced from google **/
         domains: ["lh3.googleusercontent.com"],
     },
 };
