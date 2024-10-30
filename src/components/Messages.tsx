ca24d589f9234a9e5306be3e718b45de1459b10e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-29 20:28:39 -0700

    segmented some functionality

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -2,6 +2,7 @@
 
 import {FC, useRef, useState} from "react";
 import {Message} from "@/lib/validations/messages"
+import {MessageTimeStamp} from "@/components/MessageTimeStamp";
 
 interface MessagesProps {
     initialMessages: Message[],
@@ -23,9 +24,7 @@ const Messages: FC<MessagesProps> = ({initialMessages, sessionId}) => {
                    <div className={classes.div2}>
                        <span className={classes.span}>
                            {message.text}{' '}
-                           <span className='message-date'>
-                               {message.timestamp}
-                           </span>
+                          <MessageTimeStamp unixTimestamp={message.timestamp}/>
                        </span>
                    </div>
                </div>
@@ -41,7 +40,7 @@ const listKey = (message: Message) =>{
 }
 
 const userHasNextMessage = (messages: Message[], ndx: number) =>{
-    return messages[ndx - 1]?.senderId === messages[ndx].senderId
+    return messages[ndx +1]?.senderId === messages[ndx].senderId
 }
 
 class ClassNames {
@@ -62,20 +61,21 @@ class ClassNames {
     }
 
     get div1(): string{
-        return this.className('flex items-end', 'justify-end')
+        return this.className('flex items-end', '', 'justify-end' )
     }
 
     get div2(): string{
-        return this.className('message-div-2','order-1 items-end', 'order-2 items-start' )
+
+        return this.className('message-div-2','order-1 items-start', 'order-1 items-end' )
     }
 
     bubble(isSpeechBalloon: boolean): string{
-        let currentUserStyling = 'bg-blue-800 text-white'
-        let partnerStyling = 'bg-gray-200 text-gray-900'
+        let partnerStyling = 'bg-black text-white mt-1'
+        let currentUserStyling = 'bg-white text-black outline mt-2'
 
         if (isSpeechBalloon){
-            currentUserStyling += ' rounded-bl-none'
             partnerStyling += ' rounded-br-none'
+            currentUserStyling += ' rounded-bl-none'
         }
 
         return this.className('px-4 py-2 rounded-lg inline-block', currentUserStyling, partnerStyling )
