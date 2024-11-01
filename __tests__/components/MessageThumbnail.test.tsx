708ca0a2a8d2fecb250baf90917e01f109110d9e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-10-31 17:05:53 -0700

    toggle visibility
    red
    
    green
    
    green
    
    red
    
    refactor
    
    green
    
    red
    
    toggle display
    
    red
    
    green

--- __tests__/components/MessageThumbnail.test.tsx
+++ __tests__/components/MessageThumbnail.test.tsx
@@ -4,12 +4,40 @@ import MessageThumbnail from "@/components/MessageThumbnail";
 
 describe('Tests to confirm component renders with correct attributes', () => {
     test('component Renders',()=>{
-        render(<MessageThumbnail/>)
+        render(<MessageThumbnail />)
     });
 
     test('Component has aria label "user thumbnail"',()=>{
         const {getByLabelText} = render(<MessageThumbnail/>)
         const component = getByLabelText('user thumbnail')
         expect(component).toBeInTheDocument();
+    });
+
+    test('If the component has the next message, then it should be invisible',()=>{
+        const stats = {hasNextMessage:true}
+        const {getByLabelText} = render(<MessageThumbnail userStatus={stats} />)
+        const component = getByLabelText('user thumbnail')
+        expect(component).toHaveClass(/invisible/i)
+    });
+
+    test("If the component doesn't has the next message, then it shouldn't be invisible",()=>{
+        const stats = {hasNextMessage:false}
+        const {getByLabelText} = render(<MessageThumbnail userStatus={stats}/>)
+        const component = getByLabelText('user thumbnail')
+        expect(component).not.toHaveClass(/invisible/i)
+    });
+
+    test("If the user is the current user, then the component should have classname order-2",()=>{
+        const stats = {hasNextMessage:false, currentUser: false}
+        const {getByLabelText} = render(<MessageThumbnail userStatus={stats}/>)
+        const component = getByLabelText('user thumbnail')
+        expect(component).toHaveClass(/order-2/i)
+    });
+
+    test("If the user isn't the current user, then the component should have classname order-1",()=>{
+        const stats = {hasNextMessage:false, currentUser: true}
+        const {getByLabelText} = render(<MessageThumbnail userStatus={stats}/>)
+        const component = getByLabelText('user thumbnail')
+        expect(component).toHaveClass(/order-1/i)
     });
 })
--- src/components/MessageThumbnail.tsx
+++ src/components/MessageThumbnail.tsx
@@ -1,7 +1,25 @@
 import {FC} from "react";
 
-const MessageThumbnail:FC = ()=>{
-    return<div aria-label='user thumbnail'/>
+interface userStatus{
+    hasNextMessage?: boolean;
+    currentUser?: boolean
 }
+
+interface MessageThumbnailProps {
+   userStatus?: userStatus;
+}
+
+const styling = (userStatus: userStatus| undefined) => {
+    if (userStatus){
+        return `order-${userStatus.currentUser? 1 : 2} ${userStatus.hasNextMessage? 'invisible': null}`
+    }
+    return ''
+}
+
+const MessageThumbnail: FC<MessageThumbnailProps> = ({userStatus}) => {
+    return <div
+        aria-label="user thumbnail"
+        className={styling(userStatus)}/>;
+};
 
 export default MessageThumbnail;
