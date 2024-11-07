5ce8daebf623f60de3cfaf740badf8a7d788388a
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-06 17:21:30 -0800

    green

--- src/components/FriendRequests/helpers.ts
+++ src/components/FriendRequests/helpers.ts
@@ -2,5 +2,5 @@ import {getPusherClient} from "@/lib/pusher";
 
 export default function subscribeToPusherClient (){
     const pusherClient = getPusherClient();
-    pusherClient.subscribe('stub');
+    pusherClient.subscribe('user:12345:incoming_friend_requests');
 }
