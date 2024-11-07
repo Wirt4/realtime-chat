8d49b17e3ed9a2f6f838b46b562f3acb7cf6d2eb
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-06 17:15:15 -0800

    red

--- __tests__/components/FriendRequests/helpers.test.ts
+++ __tests__/components/FriendRequests/helpers.test.ts
@@ -1,1 +1,14 @@
-describe(' subscribeToPusherClient tests', ()=>{})
+import subscribeToPusherClient from "@/components/FriendRequests/helpers";
+import {getPusherClient} from "@/lib/pusher";
+jest.mock("@/lib/pusher",()=>({
+    getPusherClient: jest.fn()
+}));
+
+describe('subscribeToPusherClient tests', ()=>{
+    test('calling function should call PusherClient.subscribe', ()=>{
+        const subscribeSpy = jest.fn();
+        (getPusherClient as jest.Mock).mockReturnValue({subscribe: subscribeSpy});
+        subscribeToPusherClient()
+        expect(subscribeSpy).toHaveBeenCalled();
+    })
+})
