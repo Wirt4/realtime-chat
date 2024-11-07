1d5c0a479357b4719fdd30bfd13c674c07516f1d
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-06 17:20:56 -0800

    red

--- __tests__/components/FriendRequests/helpers.test.ts
+++ __tests__/components/FriendRequests/helpers.test.ts
@@ -12,4 +12,10 @@ describe('subscribeToPusherClient tests', ()=>{
         subscribeToPusherClient();
         expect(subscribeSpy).toHaveBeenCalled();
     })
+    test('if the sessionID is 12345, then subscribe is called with user:12345:incoming_friend_requests',()=>{
+        const subscribeSpy = jest.fn();
+        (getPusherClient as jest.Mock).mockReturnValue({subscribe: subscribeSpy});
+        subscribeToPusherClient();
+        expect(subscribeSpy).toHaveBeenCalledWith('user:12345:incoming_friend_requests');
+    })
 })
