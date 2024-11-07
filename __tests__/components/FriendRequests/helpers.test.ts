121b8cc756a8f7a8b9e85197db671ee7d759e2bf
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-06 18:04:44 -0800

    red: format for pusher

--- __tests__/components/FriendRequests/helpers.test.ts
+++ __tests__/components/FriendRequests/helpers.test.ts
@@ -20,11 +20,11 @@ describe('subscribeToPusherClient tests', ()=>{
 
     test('if the sessionID is 12345, then subscribe is called with user:12345:incoming_friend_requests',()=>{
         subscribeToPusherClient('12345');
-        expect(subscribeSpy).toHaveBeenCalledWith('user:12345:incoming_friend_requests');
+        expect(subscribeSpy).toHaveBeenCalledWith('user__12345__incoming_friend_requests');
     })
 
     test('if the sessionID is 54321 , then subscribe is called with user:54321:incoming_friend_requests',()=>{
         subscribeToPusherClient('54321');
-        expect(subscribeSpy).toHaveBeenCalledWith('user:54321:incoming_friend_requests');
+        expect(subscribeSpy).toHaveBeenCalledWith('user__54321__incoming_friend_requests');
     })
 })
