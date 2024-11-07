d4d3109b5df4caf4b79263026881d5cf733675ec
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-06 18:39:35 -0800

    refactor

--- src/components/FriendRequests/helpers.ts
+++ src/components/FriendRequests/helpers.ts
@@ -6,16 +6,18 @@ export default class PusherClientHandler{
     private readonly _sessionId: string
     private _pusherClient: PusherClient;
     private readonly _subscribeQuery: string;
+    private readonly _bindField: string;
 
     constructor(sessionId:string){
         this._sessionId = sessionId
         this._pusherClient = getPusherClient()
         this._subscribeQuery = QueryBuilder.incomingFriendRequests(this._sessionId).replace(/:/g, '__')
+        this._bindField = 'incoming_friend_requests'
     }
 
     subscribeToPusherClient (){
         this._pusherClient.subscribe(this._subscribeQuery);
-        this._pusherClient.bind('incoming_friend_requests', this.friendRequestHandler)
+        this._pusherClient.bind(this._bindField, this.friendRequestHandler)
         return this.tearDown
     }
 
@@ -23,6 +25,6 @@ export default class PusherClientHandler{
 
     tearDown(){
         this._pusherClient.unsubscribe(this._subscribeQuery);
-        this._pusherClient.unbind('incoming_friend_requests');
+        this._pusherClient.unbind(this._bindField);
     }
 }
