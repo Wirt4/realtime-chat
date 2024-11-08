83459b390f9607ecb545b9156396efdb4af0c9f9
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-07 23:13:48 -0800

    refactor

--- __tests__/components/FriendRequestSidebarOptions/helpers.test.ts
+++ __tests__/components/FriendRequestSidebarOptions/helpers.test.ts
@@ -7,12 +7,10 @@ jest.mock("@/lib/pusher",()=>({
 
 describe('PusherClientHandler tests', () => {
     let subscribeSpy: jest.SpyInstance;
-    let bindSpy: jest.SpyInstance;
 
     beforeEach(()=>{
         jest.resetAllMocks();
-        bindSpy = jest.fn();
-        subscribeSpy = jest.fn(()=> {return { bind: bindSpy}});
+        subscribeSpy = jest.fn();
         (getPusherClient as jest.Mock).mockReturnValue({subscribe: subscribeSpy});
     })
     test('calling function should call PusherClient.subscribe', ()=>{
