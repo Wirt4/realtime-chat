9974ed96879e075d8e30a9206fd10bcdbacdf94b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-10 18:57:27 -0800

    red

--- __tests__/app/api/friends/deny/route.test.ts
+++ __tests__/app/api/friends/deny/route.test.ts
@@ -49,7 +49,8 @@ describe('error cases', ()=>{
         expect(response.body?.toString()).not.toEqual('Unauthorized');
     });
 
-    test('given nothing throws an uncaught error, when the api is called then it should return a Response Object', async ()=>{
+    test('given nothing throws an uncaught error, when the api is called, ' +
+        'then it should return a Response Object', async ()=>{
         const request = new Request('/api/friends/accept', {
             method: 'POST',
             body: JSON.stringify({ id: 'validID' }),
@@ -60,7 +61,8 @@ describe('error cases', ()=>{
         expect(typeof response).toEqual(typeof expectedType);
     });
 
-    test("given the session works but the  parameter isn't formatted correctly, when the api is called, it should return a 422", async()=>{
+    test("given the session works but the  parameter isn't formatted correctly, " +
+        "when the api is called, it should return a 422", async()=>{
         (getServerSession as jest.Mock).mockResolvedValue({user:{id:'stub'}});
         const request = new Request('/api/friends/accept', {
             method: 'POST',
@@ -72,7 +74,8 @@ describe('error cases', ()=>{
         expect(response.body?.toString()).toEqual('Invalid Request payload');
     })
 
-    test("given the session works and the parameter is formatted correctly, when the api is called, then it won't return a 422", async()=>{
+    test("given the session works and the parameter is formatted correctly, when the api is called, " +
+        "then it won't return a 422", async()=>{
         (getServerSession as jest.Mock).mockResolvedValue({user:{id:'stub'}});
         const request = new Request('/api/friends/accept', {
             method: 'POST',
@@ -84,7 +87,8 @@ describe('error cases', ()=>{
         expect(response.body?.toString()).not.toEqual('Invalid Request payload');
     })
 
-    test("given the session works, the parameter is formatted correctly and the database throws, when the api is called, then it won't return a 400", async()=>{
+    test("given the session works, the parameter is formatted correctly and the database throws, " +
+        "when the api is called, then it won't return a 400", async()=>{
         (getServerSession as jest.Mock).mockResolvedValue({user:{id:'stub'}});
         const request = new Request('/api/friends/accept', {
             method: 'POST',
@@ -97,5 +101,18 @@ describe('error cases', ()=>{
         const response = await POST(request);
         expect(response.status).toEqual(424);
         expect(response.body?.toString()).toEqual('Redis Error');
+    })
+
+    test("given the session works, the parameter is formatted correctly and the database doesn't throw, " +
+        "when the api is called, then it will return a 200", async()=>{
+        (getServerSession as jest.Mock).mockResolvedValue({user:{id:'stub'}});
+        const request = new Request('/api/friends/accept', {
+            method: 'POST',
+            body: JSON.stringify({ id: 'validID' }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const response = await POST(request);
+        expect(response.status).toEqual(200);
+        expect(response.body?.toString()).toEqual('OK');
     })
 })
--- src/app/api/friends/deny/route.ts
+++ src/app/api/friends/deny/route.ts
@@ -5,19 +5,19 @@ export async function POST(req:Request) {
     const session = await myGetServerSession()
     
     if (!session) {
-        return respondWith('Unauthorized', 401)
+        return returnResponse('Unauthorized', 401)
     }
 
     try{
         const body = await req.json()
         z.object({ id: z.string() }).parse(body)
     }catch{
-        return respondWith('Invalid Request payload', 421)
+        return returnResponse('Invalid Request payload', 421)
     }
 
-    return respondWith('Redis Error', 424)
+    return returnResponse('Redis Error', 424)
 }
 
-function respondWith(message: string, status: number): Response {
+function returnResponse(message: string, status: number): Response {
     return new Response(message, {status})
 }
