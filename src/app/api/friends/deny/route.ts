e7a01231ffae1ae64f63dff556a366115c42011b
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-10 18:38:47 -0800

    refactor

--- src/app/api/friends/deny/route.ts
+++ src/app/api/friends/deny/route.ts
@@ -2,14 +2,17 @@ import myGetServerSession from "@/lib/myGetServerSession";
 import {z} from "zod";
 
 export async function POST(req:Request) {
-    if ( await myGetServerSession()){
-        try{
-            const body = await req.json()
-           z.object({ id: z.string() }).parse(body)
-        }catch(error){
-            return new Response('Invalid Request payload', { status: 421 })
-        }
-        return new Response('OK')
+    const session = await myGetServerSession()
+    if (!session) {
+        return new Response('Unauthorized', { status: 401 })
     }
-    return new Response('Unauthorized', { status: 401 })
+
+    try{
+        const body = await req.json()
+        z.object({ id: z.string() }).parse(body)
+    }catch{
+        return new Response('Invalid Request payload', { status: 421 })
+    }
+
+    return new Response('OK')
 }
