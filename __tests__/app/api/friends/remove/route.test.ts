2607ee0549719c12916908d1a870e1abca97f7a3
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-11-13 22:01:00 -0800

    green

--- __tests__/app/api/friends/remove/route.test.ts
+++ __tests__/app/api/friends/remove/route.test.ts
@@ -4,16 +4,23 @@ jest.mock("@/lib/db");
 
 describe('Functionality Tests', () => {
     test('Given that the endpoint accepts a parameter of {idToRemove: string}: When the endpoint is called with no body in the request, then it returns a 422 ', async ()=>{
-        const request = new Request('/api/friends/remove')
+        const request = new Request("/api/friends/remove",
+            {
+                method: "POST",
+                body: "",
+                headers: { 'Content-Type': 'application/json' }
+            });
         const result = await POST(request)
         expect(result.status).toEqual(422)
     })
 
     test('Given that the endpoint accepts a parameter of {idToRemove: string}: When the endpoint is called with a correct request, then it does not return a 422 ', async ()=>{
-        const request = new Request("/api/friends/remove", {
+        const request = new Request("/api/friends/remove",
+            {
             method: "POST",
-            body: "{\"idToRemove\": \"validId\"}",
-        });
+            body: JSON.stringify({ idToRemove: '1966' }),
+                headers: { 'Content-Type': 'application/json' }
+            });
         const result = await POST(request)
         expect(result.status).not.toEqual(422)
     })
--- src/app/api/friends/remove/route.ts
+++ src/app/api/friends/remove/route.ts
@@ -1,3 +1,11 @@
-export async function POST(req: Request) {
-    return new Response('text', {status: 422});
+import {z} from "zod";
+
+export async function POST(request: Request) {
+    try{
+        const body = await request.json();
+        z.object({idToRemove: z.string()}).parse(body);
+    }catch{
+        return new Response('text', {status: 422});
+    }
+    return new Response('OK');
 }
