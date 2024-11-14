2ee56027c01d490e10417122e98837c90443db0d
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-11-13 23:06:57 -0800

    green

--- src/app/api/friends/remove/route.ts
+++ src/app/api/friends/remove/route.ts
@@ -3,19 +3,21 @@ import {getServerSession} from "next-auth";
 import fetchRedis from "@/helpers/redis";
 
 export async function POST(request: Request) {
+    let targetId: string
     try{
-        z.object({idToRemove: z.string()}).parse(await request.json());
+       const {idToRemove} =  z.object({idToRemove: z.string()}).parse(await request.json());
+       targetId = idToRemove;
     }catch{
         return respond("Invalid Input", 422)
     }
 
     const session = await getServerSession()
 
-    if (!session) {
+    if (!session?.user?.id) {
         return respond('Unauthorized', 401)
     }
 
-    await fetchRedis('sismember', "user:1977:friends", "1966")
+    await fetchRedis('sismember', `user:${session.user.id}:friends`, targetId)
     return respond('Not Friends', 400);
 }
 
