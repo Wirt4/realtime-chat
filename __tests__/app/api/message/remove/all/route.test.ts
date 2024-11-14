a6dde53772275ef51c439f52303fbafd53d84597
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-11-14 09:46:09 -0800

    red

--- __tests__/app/api/message/remove/all/route.test.ts
+++ __tests__/app/api/message/remove/all/route.test.ts
@@ -5,10 +5,15 @@ jest.mock('next-auth', () => ({
     getServerSession: jest.fn(),
 }));
 
-describe('Remove all routes', () => {
+describe('Remove all messages', () => {
     test('Given the server session is invalid when the endpoint is called then it returns a 401',async ()=>{
         (getServerSession as jest.Mock).mockResolvedValue(null)
         const response = await POST()
         expect(response.status).toBe(401)
+    })
+    test('Given the chatId is missing from the payload, when the endpoint is called, then it returns a 422', async ()=>{
+        (getServerSession as jest.Mock).mockResolvedValue({user:{id:'stub'}})
+        const response = await POST()
+        expect(response.status).toBe(422)
     })
 })
