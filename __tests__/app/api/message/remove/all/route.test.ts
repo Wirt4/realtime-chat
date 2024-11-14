ee58e6559f13e89a7f6df795cefd1d62d58baab7
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-11-14 10:02:30 -0800

    green

--- __tests__/app/api/message/remove/all/route.test.ts
+++ __tests__/app/api/message/remove/all/route.test.ts
@@ -8,19 +8,41 @@ jest.mock('next-auth', () => ({
 describe('Remove all messages', () => {
     test('Given the server session is invalid when the endpoint is called then it returns a 401',async ()=>{
         (getServerSession as jest.Mock).mockResolvedValue(null)
-        const response = await POST()
+
+        const request = new Request("/api/message/remove/all",
+            {
+                method: "POST",
+                body: JSON.stringify({ chatId: 'alpha-beta' }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+        const response = await POST(request)
         expect(response.status).toBe(401)
     })
 
     test('Given the chatId is missing from the payload, when the endpoint is called, then it returns a 422', async ()=>{
         (getServerSession as jest.Mock).mockResolvedValue({user:{id:'stub'}})
-        const response = await POST()
+        const request = new Request("/api/message/remove/all",
+            {
+                method: "POST",
+                body: "{}",
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+        const response = await POST(request)
         expect(response.status).toBe(422)
     })
 
     test('Given the chatId is "alpha--beta" and the session id is "kappa", when the endpoint is called, then it returns a 401', async ()=>{
         (getServerSession as jest.Mock).mockResolvedValue({user:{id:'kappa'}})
-        const response = await POST({chatId:'alpha--beta'})
+        const request = new Request("/api/message/remove/all",
+            {
+                method: "POST",
+                body: JSON.stringify({chatId: "alpha--beta"}),
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+        const response = await POST(request)
         expect(response.status).toBe(401)
     })
 })
--- src/app/api/message/remove/all/route.ts
+++ src/app/api/message/remove/all/route.ts
@@ -1,14 +1,26 @@
 import {getServerSession} from "next-auth";
+import {z} from "zod";
 
-export async function POST() {
+export async function POST(request: Request) {
 
     const session = await getServerSession()
+    if (!session) {
+        return respond('Unauthorized', 401)
+    }
+    let chatIncludesSessionId: boolean = false;
+    try{
+        const body = await request.json()
+        const chatId = z.object({chatId: z.string()}).parse(body).chatId;
+        chatIncludesSessionId = chatId.split('__').includes(session?.user?.id as string);
+    }catch(err){
+        console.log(err)
+        return respond('Invalid Input', 422)
+    }
 
-    if (!session?.user?.id) {
+    if (!chatIncludesSessionId) {
         return respond('Unauthorized', 401)
     }
 
-    return respond('Invalid Input', 422)
 }
 
 function respond(message: string, status: number) {
