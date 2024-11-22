a53ea79b698c596ede8b73d041ebfbfcbba3f221
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-22 14:50:15 -0800

    refactor

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,9 +4,11 @@ import {FC} from "react";
 interface pageProps {}
 
 const Page: FC<pageProps> =  ({}) =>{
-    return <>
-        <title>Wirt Salthouse's Chat App</title>
-</>
+    return (
+        <>
+            <title>Wirt Salthouse's Chat App</title>
+        </>
+    )
 }
 
 export default Page
