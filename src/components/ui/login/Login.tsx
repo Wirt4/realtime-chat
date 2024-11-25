792eba9ec2a45316cf04198964da2b1c97a762c8
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-25 13:07:49 -0800

    green

--- src/components/ui/login/Login.tsx
+++ src/components/ui/login/Login.tsx
@@ -12,12 +12,12 @@ import Link from "next/link";
 const Login:FC =()=>{
     const [isLoading, setIsLoading] = useState<boolean>(false)
     return <div className='p-5 justify-center'>
-        <h1 className='flex'>Wirt's Realtime Chat<MessagesSquare  height="40"/></h1>
+        <h1 className='flex'>Wirt's Realtime Chat<MessagesSquare/></h1>
         <div className='md:flex'>
             <Image alt="Wirt Salthouse" width={275} height={275} src="/wirt_salthouse.jpg" className='rounded-sm'/>
             <div className='flex items-end'>
             <div className='md:px-10'>
-                <h2 className='text-2xl font-bold tracking-tight pt-10'>Login with Google</h2>
+                <h2 className='text-2xl font-bold tracking-tight pt-10'>Sign in with Google</h2>
                 <Button isLoading={isLoading} type='button' className='login-button' onClick={Utils.loginWithGoogle(setIsLoading)}>
                     {isLoading? null: <GoogleLogo/>} Google
                 </Button>
@@ -30,32 +30,6 @@ const Login:FC =()=>{
             </div>
         </div>
     </div>
-}
-
-const temp: FC = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const Logo = Icons.Logo
-    return <div className= 'login-page'>
-        <div className='login-body'>
-            <h1>Wirt's Realtime Chat</h1>
-            <div className='login-body'><Image alt="Wirt Salthouse" width={275} height={275} src="/wirt_salthouse.jpg"/></div>
-        </div>
-        <Link href='/login'>Github</Link>
-            <div className='login-body'>
-                <div>
-                    <Logo className='login-logo'/>
-                    <h2 className='login-h2'>
-                        Sign in with Google
-                    </h2>
-                    <Button isLoading={isLoading} type='button'
-                        className='login-button'
-                        onClick={Utils.loginWithGoogle(setIsLoading)}>
-                            {isLoading? null: <GoogleLogo/>}
-                            Google
-                    </Button>
-                </div>
-            </div>
-        </div>
 }
 
 export default Login
