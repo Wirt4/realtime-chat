f459204712bc385ad0c8b3801dc221d02bfce3d7
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-25 14:45:14 -0800

    refactor

--- src/components/ui/login/Login.tsx
+++ src/components/ui/login/Login.tsx
@@ -9,28 +9,49 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Login:FC =()=>{
-    const [isLoading, setIsLoading] = useState<boolean>(false)
     return <div className='login-page'>
         <h1 className='flex'>Wirt's Realtime Chat<MessagesSquare/></h1>
         <div className='md:flex'>
-            <Image alt="Wirt Salthouse" width={275} height={275} src="/wirt_salthouse.jpg" className='rounded'/>
+            <Pic/>
             <div className='my-justify-end'>
             <div className='md:px-10'>
                 <h2>Sign in with Google</h2>
-                <Button isLoading={isLoading} type='button' className='login-button' onClick={Utils.loginWithGoogle(setIsLoading)}>
-                    {isLoading? null: <GoogleLogo/>} Google
-                </Button>
-                <ul className='pt-5'>
-                    <li>Add friends and chat</li>
-                    <li>Mobile-first design</li>
-                    <li><Link href='https://github.com/Wirt4/realtime-chat'>
-                        View source code at <strong><u>Github</u></strong></Link>
-                    </li>
-                </ul>
+                <GoogleButton/>
+                <Description/>
                 </div>
             </div>
         </div>
     </div>
+}
+
+const Pic: FC=()=>{
+    const width = 275
+    const height = 275
+    return <Image alt="Wirt Salthouse"
+                  width={width}
+                  height={height}
+                  src="/wirt_salthouse.jpg"
+                  className='rounded'/>
+}
+
+const GoogleButton: FC = ()=>{
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    return <Button isLoading={isLoading} type='button'
+                   className='login-button'
+                   onClick={Utils.loginWithGoogle(setIsLoading)}>
+        {isLoading? null: <GoogleLogo/>} Google
+    </Button>
+}
+
+const Description: FC = ()=> {
+    return <ul className='pt-5'>
+        <li>Add friends and chat</li>
+        <li>Mobile-first design</li>
+        <li><Link href='https://github.com/Wirt4/realtime-chat'>
+            View source code at <strong><u>Github</u></strong></Link>
+        </li>
+    </ul>
 }
 
 export default Login
