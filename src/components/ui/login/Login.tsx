57b41577f00a8027774a0c07dbc076084af2c59f
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-25 14:39:03 -0800

    migrate styles to global

--- src/components/ui/login/Login.tsx
+++ src/components/ui/login/Login.tsx
@@ -10,20 +10,22 @@ import Link from "next/link";
 
 const Login:FC =()=>{
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    return <div className='p-5 justify-items-center'>
+    return <div className='login-page'>
         <h1 className='flex'>Wirt's Realtime Chat<MessagesSquare/></h1>
         <div className='md:flex'>
-            <Image alt="Wirt Salthouse" width={275} height={275} src="/wirt_salthouse.jpg" className='rounded-sm'/>
-            <div className='flex items-end'>
+            <Image alt="Wirt Salthouse" width={275} height={275} src="/wirt_salthouse.jpg" className='rounded'/>
+            <div className='my-justify-end'>
             <div className='md:px-10'>
-                <h2 className='text-2xl font-bold tracking-tight pt-10'>Sign in with Google</h2>
+                <h2>Sign in with Google</h2>
                 <Button isLoading={isLoading} type='button' className='login-button' onClick={Utils.loginWithGoogle(setIsLoading)}>
                     {isLoading? null: <GoogleLogo/>} Google
                 </Button>
                 <ul className='pt-5'>
                     <li>Add friends and chat</li>
                     <li>Mobile-first design</li>
-                    <li><Link href='https://github.com/Wirt4/realtime-chat'>View source code at <strong><u>Github</u></strong></Link></li>
+                    <li><Link href='https://github.com/Wirt4/realtime-chat'>
+                        View source code at <strong><u>Github</u></strong></Link>
+                    </li>
                 </ul>
                 </div>
             </div>
