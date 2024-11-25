9c41c5a96fecd23fb77ccad3d68047b2a3fedc28
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-25 14:33:23 -0800

    adjust test page

--- __tests__/components/ui/login.test.tsx
+++ __tests__/components/ui/login.test.tsx
@@ -6,10 +6,9 @@ describe('Login Component', () => {
     afterAll(()=>{
         jest.restoreAllMocks()
     })
-    test('renders the login page', () => {
-        const {getByText} = render(<Login />);
 
-        // Check if the sign-in heading is rendered
+    test('Page contains text "Sign in with Google"', () => {
+        const {getByText} = render(<Login />);
         const title = getByText("Sign in with Google")
         expect(title).toBeInTheDocument();
     })
@@ -22,33 +21,25 @@ describe('Login Component', () => {
 
     test('Contains a picture of the Dev', async () => {
         const { getByAltText } = await render(<Login />);
-
         const image = getByAltText("Wirt Salthouse");
-
         expect(image).toHaveAttribute('src', expect.stringContaining('wirt_salthouse.jpg'))
     });
 
     test('renders the heading', () => {
         const {getByRole} = render(<Login />);
-
         const heading = getByRole('heading', { name: "Wirt's Realtime Chat" });
-
         expect(heading).toBeInTheDocument();
     });
 
     test('Contains a link to the github page', () => {
         const {getByRole} = render(<Login />);
-
         const linkElement = getByRole('link', { name: /Github/i });
-
         expect(linkElement).toBeInTheDocument();
     });
 
     test('Contains a link to the github page', () => {
         const {getByRole} = render(<Login />);
-
         const linkElement = getByRole('link', { name: /Github/i });
-
         expect(linkElement).toHaveAttribute('href', expect.stringContaining('https://github.com/Wirt4/realtime-chat'))
     });
 })
