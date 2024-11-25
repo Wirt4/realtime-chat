233524a7b753cbfbad7f37a157337a2960f4e9b3
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-11-25 14:45:44 -0800

    Update login page (#79)
    * green
    
    * refactor
    
    * refactor
    
    * red
    
    * green
    
    * red
    
    * green
    
    * update styling
    
    * green
    
    * refactor
    
    * red
    
    * green
    
    * adjust test page
    
    * remove unnecessary work
    
    * migrate styles to global
    
    * refactor

--- __tests__/components/ui/login.test.tsx
+++ __tests__/components/ui/login.test.tsx
@@ -1,21 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import Login from '@/components/ui/login/Login';
 import '@testing-library/jest-dom'
-import {Utils} from "@/lib/utils";
+import { render } from '@testing-library/react';
+import Login from '@/components/ui/login/Login';
 
 describe('Login Component', () => {
     afterAll(()=>{
         jest.restoreAllMocks()
     })
-    test('renders the login page', () => {
-        jest.spyOn(Utils, 'loginWithGoogle').mockImplementation(jest.fn())
-        render(<Login />);
 
-        // Check if the sign-in heading is rendered
-        expect(screen.getByText('Sign in with Google')).toBeInTheDocument();
-
-        // Check if the Google button is rendered
-        const googleButton = screen.getByText('Google');
-        expect(googleButton).toBeInTheDocument();
+    test('Page contains text "Sign in with Google"', () => {
+        const {getByText} = render(<Login />);
+        const title = getByText("Sign in with Google")
+        expect(title).toBeInTheDocument();
     })
+
+    test('Renders the google button',()=>{
+        const {getByText} = render(<Login />);
+        const button = getByText('Google')
+        expect(button).toBeInTheDocument();
+    })
+
+    test('Contains a picture of the Dev', async () => {
+        const { getByAltText } = await render(<Login />);
+        const image = getByAltText("Wirt Salthouse");
+        expect(image).toHaveAttribute('src', expect.stringContaining('wirt_salthouse.jpg'))
+    });
+
+    test('renders the heading', () => {
+        const {getByRole} = render(<Login />);
+        const heading = getByRole('heading', { name: "Wirt's Realtime Chat" });
+        expect(heading).toBeInTheDocument();
+    });
+
+    test('Contains a link to the github page', () => {
+        const {getByRole} = render(<Login />);
+        const linkElement = getByRole('link', { name: /Github/i });
+        expect(linkElement).toBeInTheDocument();
+    });
+
+    test('Contains a link to the github page', () => {
+        const {getByRole} = render(<Login />);
+        const linkElement = getByRole('link', { name: /Github/i });
+        expect(linkElement).toHaveAttribute('href', expect.stringContaining('https://github.com/Wirt4/realtime-chat'))
+    });
 })
+
--- src/components/ui/login/Login.tsx
+++ src/components/ui/login/Login.tsx
@@ -4,27 +4,54 @@ import {FC, useState} from 'react'
 import Button from "@/components/ui/button/Button";
 import GoogleLogo from "@/assets/svgPaths/googleSVG";
 import {Utils} from "@/lib/utils";
-import {Icons} from "@/components/Icons";
+import {MessagesSquare} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-const Login:FC = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const Logo = Icons.Logo
-    return <div className= 'login-page'>
-            <div className='login-body'>
-                <div>
-                    <Logo className='login-logo'/>
-                    <h2 className='login-h2'>
-                        Sign in with Google
-                    </h2>
+const Login:FC =()=>{
+    return <div className='login-page'>
+        <h1 className='flex'>Wirt's Realtime Chat<MessagesSquare/></h1>
+        <div className='md:flex'>
+            <Pic/>
+            <div className='my-justify-end'>
+            <div className='md:px-10'>
+                <h2>Sign in with Google</h2>
+                <GoogleButton/>
+                <Description/>
                 </div>
-                    <Button isLoading={isLoading} type='button'
-                        className='login-button'
-                        onClick={Utils.loginWithGoogle(setIsLoading)}>
-                            {isLoading? null: <GoogleLogo/>}
-                            Google
-                    </Button>
             </div>
         </div>
+    </div>
+}
+
+const Pic: FC=()=>{
+    const width = 275
+    const height = 275
+    return <Image alt="Wirt Salthouse"
+                  width={width}
+                  height={height}
+                  src="/wirt_salthouse.jpg"
+                  className='rounded'/>
+}
+
+const GoogleButton: FC = ()=>{
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    return <Button isLoading={isLoading} type='button'
+                   className='login-button'
+                   onClick={Utils.loginWithGoogle(setIsLoading)}>
+        {isLoading? null: <GoogleLogo/>} Google
+    </Button>
+}
+
+const Description: FC = ()=> {
+    return <ul className='pt-5'>
+        <li>Add friends and chat</li>
+        <li>Mobile-first design</li>
+        <li><Link href='https://github.com/Wirt4/realtime-chat'>
+            View source code at <strong><u>Github</u></strong></Link>
+        </li>
+    </ul>
 }
 
 export default Login
