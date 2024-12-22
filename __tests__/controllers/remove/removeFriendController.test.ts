5a2dd66539431b70ebdbec68e0399a149ca88997
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-21 20:34:29 -0800

    still green

--- __tests__/controllers/remove/removeFriendController.test.ts
+++ __tests__/controllers/remove/removeFriendController.test.ts
@@ -3,7 +3,6 @@ import {FriendsRemoveController} from "@/controllers/friends/remove/controller";
 describe('Functionality Tests', () => {
     let request: Request
     let controller: FriendsRemoveController
-    let response: Response
     
     beforeEach(()=>{
         jest.clearAllMocks()
@@ -12,7 +11,7 @@ describe('Functionality Tests', () => {
                 method: "POST",
                 body: JSON.stringify({ idToRemove: '1966' }),
                 headers: { 'Content-Type': 'application/json' }
-            });
+            }) as Request
     })
     
     it("if the body isn't formatted correctly, return a 422", async () => {
@@ -21,23 +20,17 @@ describe('Functionality Tests', () => {
                 method: "POST",
                 body: "",
                 headers: { 'Content-Type': 'application/json' }
-            });
+            }) as Request
         controller =  new FriendsRemoveController()
-        response = await controller.remove(request)
+        const response = await controller.remove(request)
         expect(response.status).toBe(422)
         expect(response.body?.toString()).toEqual('Invalid Format')
     })
 
 
     it("if the body isn't formatted correctly, return a 422", async () => {
-        request = new Request("/api/friends/remove",
-            {
-                method: "POST",
-                body: JSON.stringify({ idToRemove: '1966' }),
-                headers: { 'Content-Type': 'application/json' }
-            });
         controller =  new FriendsRemoveController()
-        response = await controller.remove(request)
+        const response = await controller.remove(request)
         expect(response.status).not.toBe(422)
         expect(response.body?.toString()).not.toEqual('Invalid Format')
     })
