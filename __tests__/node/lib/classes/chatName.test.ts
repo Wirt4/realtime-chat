2c991fe92e084a823eeef28ae7ce88503b87f5bb
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-01-22 13:03:04 -0800

    green

--- __tests__/node/lib/classes/chatName.test.ts
+++ __tests__/node/lib/classes/chatName.test.ts
@@ -1,7 +1,26 @@
 import { ChatName } from '@/lib/classes/chatName/implementation';
 
 describe('ChatName tests', () => {
-    it('should be an instantiable class', () => {
-        const chatName = new ChatName();
+    it('rshould return a default title but not include the users name', () => {
+        const participants: User[] = [
+            { name: "Alfred", id: "foo", email: "bar", image: "/spam" },
+            { name: "theUser", id: "bar", email: "bar", image: "/spam" },
+            { name: "Betty", id: "foo", email: "bar", image: "/spam" },
+            { name: "Charlie", id: "foo", email: "bar", image: "/spam" },
+        ]
+        const sessionId = "bar";
+        const chatName = new ChatName(participants, sessionId);
+        expect(chatName.getChatName()).toEqual("Chat with Alfred, Betty, and Charlie");
+    })
+    it('rshould return a default title but not include the users name', () => {
+        const participants: User[] = [
+            { name: "Alfred", id: "bar", email: "bar", image: "/spam" },
+            { name: "theUser", id: "foo", email: "bar", image: "/spam" },
+            { name: "Betty", id: "foo", email: "bar", image: "/spam" },
+            { name: "Charlie", id: "foo", email: "bar", image: "/spam" },
+        ]
+        const sessionId = "bar";
+        const chatName = new ChatName(participants, sessionId);
+        expect(chatName.getChatName()).toEqual("Chat with Betty, Charlie, and theUser");
     })
 })
--- src/lib/classes/chatName/implementation.ts
+++ src/lib/classes/chatName/implementation.ts
@@ -1,7 +1,34 @@
 import { aChatName } from './abstract';
 
 export class ChatName extends aChatName {
+    private participants: User[];
+    private sessionId: string;
+    constructor(partcipants: User[], sessionId: string) {
+        super();
+        this.participants = partcipants;
+        this.sessionId = sessionId;
+    }
+
     getChatName(): string {
-        return 'ChatName';
+        let names: string[] = [];
+
+        for (let i = 0; i < this.participants.length; i++) {
+            if (this.participants[i].id !== this.sessionId) {
+                names.push(this.participants[i].name);
+            }
+        }
+        names.sort();
+
+        if (names.length === 1) {
+            return "Chat with" + names[0];
+        }
+
+        if (names.length === 2) {
+            return "Chat with" + names.join(' and ');
+        }
+
+        const lastIndex = names.length - 1;
+        names[lastIndex] = `and ${names[lastIndex]}`;
+        return "Chat with " + names.join(', ');
     }
 }
