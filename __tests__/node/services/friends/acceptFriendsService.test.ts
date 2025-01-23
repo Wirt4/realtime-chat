a390a371875574355a234fbc1f5a6ec97b63e67c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-01-22 18:24:09 -0800

    green

---
+++ __tests__/node/services/friends/acceptFriendsService.test.ts
@@ -0,0 +1,6 @@
+import { AcceptFriendsService } from '@/services/friends/accept/implementation'
+describe('acceptFriendsService', () => {
+    it('should instantiate', async () => {
+        const service = new AcceptFriendsService()
+    })
+})
