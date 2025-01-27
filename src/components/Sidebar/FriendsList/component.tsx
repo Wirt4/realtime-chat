e4b1cac62c0b9c15032b5879d167ea01810a441d
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2025-01-27 14:08:51 -0800

    Add popups (#93)
    * some basic start, now to test, refactor, and simplify
    
    * some basic popups around ui

--- src/components/Sidebar/FriendsList/component.tsx
+++ src/components/Sidebar/FriendsList/component.tsx
@@ -1,16 +1,35 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useState } from "react";
 import { FriendsListProps } from "./interface";
+import { X } from "lucide-react";
+import Button from "@/components/ui/button/Button";
 
 const FriendsList: FC<FriendsListProps> = (props) => {
     const { friends } = props;
+    const [isVisible, setIsVisible] = useState(false);
+
+    const openPopup = () => setIsVisible(true);
+    const closePopup = () => setIsVisible(false);
     return (
         <ul className='sidebar-chat-list'>
             {friends?.sort((friendA: string, friendB: string) => {
                 return friendA > friendB ? 1 : -1
             }).map((friend: string) => {
-                return (<li className='sidebar-chat-list-item'>
-                    <span className='truncate'>{friend}</span>
-                </li>)
+                return (<div><li className='sidebar-chat-list-item'>
+                    <span className='truncate' onClick={!isVisible ? openPopup : closePopup}>{friend}</span>
+                </li>
+                    {isVisible && (
+                        <li className='sidebar-chat-list-item'>
+                            <div className="flex flex-col">
+                                <ul className="flex flex-col pl-4">
+                                    <li>Chat</li>
+                                    <li>Remove friend</li>
+                                </ul>
+                            </div>
+                        </li>
+                    )}
+                </div>)
             })}
         </ul>)
 }
