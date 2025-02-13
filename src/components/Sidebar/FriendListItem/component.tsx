313218ec1751c551ac725d72b64189cf1dfb5a78
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-13 15:54:01 -0800

    space nudge

--- src/components/Sidebar/FriendListItem/component.tsx
+++ src/components/Sidebar/FriendListItem/component.tsx
@@ -5,6 +5,7 @@ import FriendListItemAPIActions from "../FriendListItemAPIActions/component";
 
 const FriendListItem: FC<FriendListItemProps> = ({ friend }) => {
     const [isVisible, setIsVisible] = useState(false);
+
     return (
         <li className='sidebar-chat-list-item'>
             <span onClick={() => setIsVisible(true)} className='truncate'>{friend.name}</span>
