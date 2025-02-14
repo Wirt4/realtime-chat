a44af306bc7ab019c450e756c09b1623547fa468
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-13 16:09:05 -0800

    refactor-again

--- src/components/Sidebar/FriendsList/component.tsx
+++ src/components/Sidebar/FriendsList/component.tsx
@@ -5,7 +5,6 @@ import FriendListItem from '../FriendListItem/component';
 
 const FriendsList: FC<FriendsListProps> = ({ friends = [] }) => {
 
-    const foo = [1, 2, 3]
     return <ul className='sidebar-chat-list'>
         {friends.map((friend) => (
             <FriendListItem key={friend.id} friend={friend} />
