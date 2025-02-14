277c3219d9cd39905debb7a5d2e720db4e7a5a13
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-14 15:00:25 -0800

    added docblock

--- src/app/api/friends/remove/route.ts
+++ src/app/api/friends/remove/route.ts
@@ -6,6 +6,35 @@ import { UserRepository } from "@/repositories/user/implementation";
 import { MessageRepository } from "@/repositories/message/removeAll/implementation";
 import { ChatProfileRepository } from "@/repositories/chatProfile/implementation";
 
+/**
+ * @swagger
+ * /api/friends/remove:
+ *   post:
+ *     summary: Remove a friend
+ *     description: Removes a friend from the user's friend list.
+ *     tags:
+ *       - Friends
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               requestId:
+ *                 type: string
+ *                 description: The ID of the user currently logged as a friend of the session user.
+ *               sessionId:
+ *                 type: string
+ *                 description: The ID of the user making the request.
+ *     responses:
+ *       200:
+ *         description: Successfully removed the friend.
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Internal server error.
+ */
 export async function POST(request: Request) {
     const controller = new RemoveFriendsController();
     const friendsRepository = new FriendsRepository(db);
--- src/app/api/message/remove/all/route.ts
+++ src/app/api/message/remove/all/route.ts
@@ -1,5 +1,32 @@
-import {MessageService} from "@/services/message/service";
-import {MessageRemoveAllController} from "@/controllers/message/remove/all/controller";
+import { MessageService } from "@/services/message/service";
+import { MessageRemoveAllController } from "@/controllers/message/remove/all/controller";
+
+
+/**
+ * /api/message/remove/all:
+ *   post:
+ *     summary: Remove all messages
+ *     description: Removes all messages from the database.
+ *     tags:
+ *       - Messages
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               chatId:
+ *                 type: string
+ *                 description: The key for the chat that requires all contents be removed.
+ *     responses:
+ *       200:
+ *         description: Successfully removed all messages.
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Internal server error.
+ */
 
 export async function POST(request: Request) {
     const service = new MessageService()
