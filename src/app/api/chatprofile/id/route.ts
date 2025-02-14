81688a5f9509d3d84a237cbb223b08549e1ad323
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-14 15:54:45 -0800

    added docbock for future api

---
+++ src/app/api/chatprofile/id/route.ts
@@ -0,0 +1,33 @@
+/**
+ * /api/chatprofile/id:
+ *   get:
+ *     summary: fetch a chat id by participant user ids
+ *     description: fetch a chat id by participant user ids
+ *     tags:
+ *       - Chatprofile
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               participants:
+ *                 type: string
+ *                 description: The IDs of the users who share the same chat.
+ *     responses:
+ *       200:
+ *         description: Successfully removed the friend.
+ *         content:
+ *          application/json:
+ *           schema:
+ *            type: object
+ *           properties:
+ *            chatId:
+ *              type: string
+ *              description: The ID of the chat with all participants, returns empty string if such a chat does not exist.
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Internal server error.
+ */
