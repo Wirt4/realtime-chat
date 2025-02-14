f850e01b7cd9f6aa24c98f05ee9d82912c263832
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-02-13 16:13:20 -0800

    some testing done to tackle this beastie

--- __tests__/jsdom/components/Sidebar/FriendsList.test.tsx
+++ __tests__/jsdom/components/Sidebar/FriendsList.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import FriendsList from '@/components/Sidebar/FriendsList/component';
 import { FriendInfo } from '@/components/Sidebar/FriendListItem/interface';
+import FriendListItem from '@/components/Sidebar/FriendListItem/component';
 
 
 jest.mock('@/components/Sidebar/FriendListItem/component', () => jest.fn(() => <li data-testid="friend-item" />));
@@ -16,32 +17,29 @@ describe('FriendList', () => {
 
     it('renders the list of friends', () => {
         render(<FriendsList friends={friends} />);
-
         expect(screen.getAllByTestId('friend-item')).toHaveLength(3);
     });
 
-    /* it('sorts friends alphabetically by name', () => {
-         render(<FriendList friends={friends} />);
-         
-         const friendItems = screen.getAllByTestId('friend-item');
-         
-         expect(FriendItem).toHaveBeenCalledWith(
-             { friend: { id: '2', name: 'Alice' } },
-             {}
-         );
-         expect(FriendItem).toHaveBeenCalledWith(
-             { friend: { id: '3', name: 'Bob' } },
-             {}
-         );
-         expect(FriendItem).toHaveBeenCalledWith(
-             { friend: { id: '1', name: 'Charlie' } },
-             {}
-         );
-     });
- 
-     it('renders correctly with an empty friends list', () => {
-         render(<FriendList friends={[]} />);
-         
-         expect(screen.queryByTestId('friend-item')).not.toBeInTheDocument();
-     });*/
+    it('sorts friends alphabetically by name', () => {
+        render(<FriendsList friends={friends} />);
+        screen.getAllByTestId('friend-item');
+
+        expect(FriendListItem).toHaveBeenCalledWith(
+            { friend: { id: '2', name: 'Alice' } },
+            {}
+        );
+        expect(FriendListItem).toHaveBeenCalledWith(
+            { friend: { id: '3', name: 'Bob' } },
+            {}
+        );
+        expect(FriendListItem).toHaveBeenCalledWith(
+            { friend: { id: '1', name: 'Charlie' } },
+            {}
+        );
+    });
+
+    it('renders correctly with an empty friends list', () => {
+        render(<FriendsList friends={[]} />);
+        expect(screen.queryByTestId('friend-item')).not.toBeInTheDocument();
+    });
 });
